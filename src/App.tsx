9eff777d15f54833d9093657a62dd81378255533
Johnny Estrada <johnnye562@yahoo.com>
2023-10-05 16:43:19 -0700

    Add routing to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
-import Sidebar from "./components/Sidebar";
-import DataDisplay from "./components/ui/DataDisplay";
-import Tabs from "./components/ui/Tabs";
-import Breadcrumbs from "./components/ui/Breadcrumbs";
-import SectionHeader from "./components/ui/SectionHeader";
-import StackedList from "./components/ui/StackedList";
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+
+const user = [{ name: 'Johnny', isAdmin: true }];
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    <>
-      {/* container */}
-      <div className="flex flex-col-reverse lg:flex-row bg-gray-100">
-        {/* Sidebar col-1 */}
-        <Sidebar />
-
-        {/*  wrapper col-2 */}
-        <div className="pt-5 flex-1 overflow-hidden">
-          {/* header */}
-          <header className="lg:ml-80 px-4">
-            <Breadcrumbs />
-            <SectionHeader />
-            <DataDisplay />
-          </header>
-
-          <div className="lg:block">
-            {/* main content */}
-            <main className="mt-6 px-7 bg-white">
-              <Tabs />
-              {/* <StackedList /> */}
-            </main>
-          </div>
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard user={user} />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
