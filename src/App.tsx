17e1c971cce5223356626faba23f55e75b5bce10
Johnny Estrada <johnnye562@yahoo.com>
2023-10-09 23:38:44 -0700

    Add additional routes to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Tasks from './pages/Tasks';
+import Notifications from './pages/Notifications';
+import Settings from './pages/Settings';
+import SignIn from './pages/SignIn';
 
 const user = [{ name: 'Johnny', isAdmin: true }];
 
@@ -13,6 +17,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Dashboard user={user} />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
