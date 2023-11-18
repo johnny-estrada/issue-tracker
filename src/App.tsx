f006bf18dd544547a0decc5dc692dfaa6e965d1d
Johnny Estrada <johnnye562@yahoo.com>
2023-11-17 18:05:47 -0800

    Add data and routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import Tasks from './pages/Tasks';
-import Notifications from './pages/Notifications';
-import Settings from './pages/Settings';
-import SignIn from './pages/SignIn';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Tasks from "./pages/Tasks";
+import Notifications from "./pages/Notifications";
+import Settings from "./pages/Settings";
+import Login from "./pages/Login";
 
-const user = [{ name: 'Johnny', isAdmin: true }];
+// const user = [{ name: 'Johnny', isAdmin: true }];
 
-export default function App() {
-  const [user, setUser] = useState(null);
+// data
+import { projects, tasks, lineChart, barChart } from "./data/index";
+
+function App() {
+  const [user, setUser] = useState({ name: "Johnny", isAdmin: true });
+  const isLoggedIn = true;
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard user={user} />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/tasks" element={<Tasks />} />
+        <Route
+          path="/"
+          element={
+            <>
+              {isLoggedIn ? (
+                <Dashboard
+                  projectData={projects}
+                  taskData={tasks}
+                  lineChartData={lineChart}
+                  barChartData={barChart}
+                />
+              ) : (
+                <Login />
+              )}
+            </>
+          }
+        />
+        <Route
+          path="/projects"
+          element={<Projects projectData={projects} taskData={tasks} />}
+        />
+        <Route path="/tasks" element={<Tasks taskData={tasks} />} />
         <Route path="/notifications" element={<Notifications />} />
-        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
