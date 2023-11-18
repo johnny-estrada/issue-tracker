0c43c6cd92faa7b48af88d3fa4f8ecb236cf4471
Johnny Estrada <johnnye562@yahoo.com>
2023-11-17 18:06:55 -0800

    Create Dashboard page component

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import Header from "../layout/Header";
+import DataDisplay from "../components/ui/DataDisplay";
+import SplitScreen from "../layout/SplitScreen";
+import Main from "../layout/Main";
+import HeaderTitle from "../components/ui/HeaderTitle";
+import TwoColumnsFull from "../layout/TwoColumnsFull";
+import ShowHideList from "../components/ui/ShowHideList";
+import LineChart from "../components/ui/charts/LineChart";
+import BarCharts from "../components/ui/charts/BarCharts";
+import SelectorList from "../components/ui/SelectorList";
+import AvatarGroup from "../components/ui/AvatarGroup";
+
+interface Props {
+  projectData: object[];
+  taskData: object[];
+  lineChartData: object[];
+  barChartData: object[];
+}
+
+const Dashboard = ({
+  projectData,
+  taskData,
+  lineChartData,
+  barChartData,
+}: Props) => {
+  const [projects, setProjects] = useState(projectData);
+  const [tasks, setTasks] = useState(taskData);
+  const [lineChart, setLineChart] = useState(lineChartData);
+  const [barChart, setBarChart] = useState(barChartData);
+
+  const projectList = projects.map((project, idx) => (
+    <SelectorList
+      key={idx}
+      id={idx}
+      // active={Number(projectIndex) === idx}
+      // onClick={toggleProjects}
+    >
+      <div className="flex min-w-0 gap-x-4">
+        <div className="flex h-12 w-12 justify-center items-center rounded-full bg-white text-black">
+          <p>BK</p>
+        </div>
+        <div className="min-w-0 flex-auto">
+          <p className="text-sm font-semibold leading-6">{project.client}</p>
+          <p className=" mt-1 flex text-xs leading-5 text-gray-400">
+            <span className="inset-x-0 -top-px bottom-0" />
+            {project.tasks} tasks &#x2022; {project.overdue} overdue
+          </p>
+        </div>
+      </div>
+      <div className="flex shrink-0 items-center gap-x-4">
+        <AvatarGroup />
+        <p className="text-gray-400">
+          {project.startDate} - {project.targetDate}
+        </p>
+      </div>
+    </SelectorList>
+  ));
+
+  const title = "Dashboard";
+
+  return (
+    <>
+      <SplitScreen>
+        <Header>
+          <HeaderTitle title={title} />
+          <DataDisplay projects={projects} />
+        </Header>
+        <Main>
+          <TwoColumnsFull>
+            <ShowHideList tasks={tasks} />
+            <>
+              <LineChart width={430} height={450} data={lineChart} />
+              <BarCharts width={430} height={450} data={barChart} />
+            </>
+            {projectList}
+          </TwoColumnsFull>
+        </Main>
+      </SplitScreen>
+    </>
+  );
+};
+
+export default Dashboard;
