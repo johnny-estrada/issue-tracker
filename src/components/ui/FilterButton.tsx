70d4ff9394605c8fdcda6e93aa7f6c3392cb1d62
Johnny Estrada <johnnye562@yahoo.com>
2023-11-17 18:19:35 -0800

    Create FilterButton component

---
+++ src/components/ui/FilterButton.tsx
@@ -0,0 +1,41 @@
+import { FunnelIcon } from "@heroicons/react/24/outline";
+
+export default function FilterButton() {
+  return (
+    <>
+      {/* <button
+          type="button"
+          className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          Button text
+        </button>
+        <button
+          type="button"
+          className="rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          Button text
+        </button>
+        <button
+          type="button"
+          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          Button text
+        </button>
+        <button
+          type="button"
+          className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          Button text
+        </button> */}
+      <button
+        type="button"
+        className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      >
+        <div className="flex gap-1">
+          <FunnelIcon className="h-5 w-5" />
+          <p>Filter</p>
+        </div>
+      </button>
+    </>
+  );
+}
