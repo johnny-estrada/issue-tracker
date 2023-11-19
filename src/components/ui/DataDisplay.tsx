a9972e52b76ec9e9faa5bd1389a3aa7d2c5bb5a4
Johnny Estrada <johnnye562@yahoo.com>
2023-11-18 18:45:38 -0800

    Move stats data from DataDisplay to data file

--- src/components/ui/DataDisplay.tsx
+++ src/components/ui/DataDisplay.tsx
@@ -1,22 +1,26 @@
-const stats = [
-    { name: 'Hours this week', stat: '32' },
-    { name: 'Project completed', stat: '78%' },
-    { name: 'Tasks due', stat: '32%' },
-    { name: 'Assigned to me', stat: '42' },
-  ]
-  
-  export default function DataDisplay() {
-    return (
-      <>
-        <dl className="mt-5 grid grid-cols-2 lg:grid-cols-4 md:grid-cols-2 gap-5">
-          {stats.map((item) => (
-            <div key={item.name} className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6  lg:max-w-60">
-              <dt className="truncate text-sm font-medium text-gray-500">{item.name}</dt>
-              <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{item.stat}</dd>
+import { stats } from "../../data/index";
+
+export default function DataDisplay() {
+  return (
+    <div>
+      <dl className="mt-5 grid grid-cols-2 lg:grid-cols-4 md:grid-cols-2 gap-5">
+        {stats.map((stat) => (
+          <div
+            key={stat.name}
+            className="overflow-hidden rounded-lg bg-white px-3 py-3 shadow sm:p-6  lg:max-w-60"
+          >
+            <dt className="truncate text-xs lg:text-sm font-medium text-gray-500 mb-4 lg:mb-8">
+              {stat.name}
+            </dt>
+            <div className="flex justify-between">
+              <dd className="mt-1 text-2xl lg:text-4xl font-semibold tracking-tight text-gray-900">
+                {stat.stat}
+              </dd>
+              <stat.icon className="w-7 h-7 mt-3 rounded-md bg-gray-100 text-gray-900 p-1" />
             </div>
-          ))}
-        </dl>
-      </>
-    )
-  }
-  
+          </div>
+        ))}
+      </dl>
+    </div>
+  );
+}
