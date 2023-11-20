3f1a1e0b6ebe4c8ee1d7ba0b734d366f20a92c31
Johnny Estrada <johnnye562@yahoo.com>
2023-11-20 14:38:51 -0800

    Add legend and update tags for LineChart

--- src/components/ui/charts/LineChart.tsx
+++ src/components/ui/charts/LineChart.tsx
@@ -6,6 +6,7 @@ import {
   YAxis,
   Tooltip,
   ResponsiveContainer,
+  Legend,
 } from "recharts";
 
 interface Props {
@@ -16,41 +17,49 @@ interface Props {
 
 function LineChart({ width, height, data }: Props) {
   return (
-    <ResponsiveContainer width="50%" height={200}>
-      <AreaChart width={width} height={height} data={data}>
-        <defs>
-          <linearGradient
-            id="colorUv"
-            x1="0"
-            y1="0"
-            x2="0"
-            y2="1"
-          ></linearGradient>
-          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#FF7A50" stopOpacity={0.6} />
-            <stop offset="95%" stopColor="#FF7A50" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        <XAxis dataKey="name" />
-        <YAxis dataKey="amt" />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Area
-          type="monotone"
-          dataKey="uv"
-          stroke="#000"
-          fillOpacity={1}
-          fill="url(#colorUv)"
-        />
-        <Area
-          type="monotone"
-          dataKey="pv"
-          stroke="#FF7A50"
-          fillOpacity={1}
-          fill="url(#colorPv)"
-        />
-      </AreaChart>
-    </ResponsiveContainer>
+    <>
+      <ResponsiveContainer width="50%" height={205}>
+        <AreaChart
+          width={width}
+          height={height}
+          data={data}
+          margin={{ top: 0, right: 30, left: -35, bottom: -25 }}
+        >
+          <Legend verticalAlign="bottom" height={36} />
+          <defs>
+            <linearGradient
+              id="colorUv"
+              x1="0"
+              y1="0"
+              x2="0"
+              y2="1"
+            ></linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#FF7A50" stopOpacity={0.6} />
+              <stop offset="95%" stopColor="#FF7A50" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" />
+          <YAxis dataKey="amt" />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="created"
+            stroke="#000"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+          <Area
+            type="monotone"
+            dataKey="completed"
+            stroke="#FF7A50"
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </>
   );
 }
 
