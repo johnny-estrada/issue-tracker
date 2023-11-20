f7918bf7a040ad1ba4bff2319671041a9e5cb76e
Johnny Estrada <johnnye562@yahoo.com>
2023-11-19 18:02:16 -0800

    Refactor Tasks page to use taskList data

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/solid";
 import SplitScreen from "../layout/SplitScreen";
 import Header from "../layout/Header";
@@ -6,9 +6,10 @@ import TwoColumns from "../layout/TwoColumns";
 import Main from "../layout/Main";
 import HeaderTitle from "../components/ui/HeaderTitle";
 import ButtonGroup from "../components/ui/ButtonGroup";
+import AvatarGroup from "../components/ui/AvatarGroup";
 import FilterButton from "../components/ui/FilterButton";
-import Details from "../components/ui/Details";
 import SelectorList from "../components/ui/SelectorList";
+import ListMenu from "../components/ui/ListMenu";
 import FlatBadge from "../components/ui/FlatBadge";
 
 interface Props {
@@ -16,35 +17,29 @@ interface Props {
 }
 
 const Tasks = ({ projectData }: Props) => {
-  const [projects, setProject] = useState(projectData);
+  const [projects, setProjects] = useState(projectData);
+  const [projectIndex, setProjectIndex] = useState(0);
   const [taskIndex, setTaskIndex] = useState(0);
 
   function toggleTasks(e) {
-    const i = Number(e.currentTarget.id);
-    setTaskIndex(i);
+    const taskId = Number(e.currentTarget.id);
+    const foundTask = projects.reduce((acc, project) => {
+      const taskIndex = project.tasksList.findIndex(
+        (task) => task.id === taskId,
+      );
+      if (taskIndex !== -1) {
+        return { project, taskIndex };
+      }
+      return acc;
+    }, null);
+
+    if (foundTask) {
+      const { project, taskIndex } = foundTask;
+      const projectIndex = projects.indexOf(project);
+      setProjectIndex(projectIndex);
+      setTaskIndex(taskIndex);
+    }
   }
-
-  // const taskList = projects.map((project) => (
-  //   project.tasksList.map((task, idx) => (
-
-  //   <SelectorList key={idx} id={idx}>
-  //     <div className="flex min-w-0 gap-x-4">
-  //       <div className="min-w-0 flex-auto">
-  //         <p className=" mt-1 flex text-xs leading-5 text-gray-400">
-  //           <span className="inset-x-0 -top-px bottom-0" />
-  //           ID PL-{task.id}
-  //         </p>
-  //         <p className="text-sm font-semibold leading-6">{task.title}</p>
-  //       </div>
-  //     </div>
-  //     <div className="flex shrink-0 items-center gap-x-4">
-  //       <FlatBadge />
-  //       <p className="text-gray-400">
-  //         <ChevronRightIcon className="h-5 w-5" />
-  //       </p>
-  //     </div>
-  //   </SelectorList>
-  // )))
 
   const title = "Tasks";
 
@@ -64,7 +59,7 @@ const Tasks = ({ projectData }: Props) => {
             <h2 className="sr-only" id="projects">
               All Projects
             </h2>
-            <div className="flex  justify-between">
+            <div className="flex justify-between">
               <h2 className="text-xl pb-4">{title}</h2>
               <div className="flex gap-5 items-center">
                 <a href="#" className="hover:bg-gray-100 rounded-full p-3">
@@ -77,11 +72,16 @@ const Tasks = ({ projectData }: Props) => {
                 </a>
               </div>
             </div>
-            <ul className="flex flex-col gap-3">
-              {projects.map((project) => (
-                <Fragment key={project.id}>
-                  {project.tasksList.map((task) => (
-                    <SelectorList key={task.id}>
+            <ul className="flex flex-col gap-3 overflow-hidden py-4">
+              {projects.map((project, i) => (
+                <React.Fragment key={project.id}>
+                  {project.tasksList.map((task, idx) => (
+                    <SelectorList
+                      key={task.id}
+                      id={task.id}
+                      active={projectIndex === i && taskIndex === idx}
+                      onClick={(e) => toggleTasks(e)}
+                    >
                       <div className="flex">
                         <div>
                           <p className=" mt-1 flex text-xs leading-5 text-gray-400">
@@ -89,29 +89,83 @@ const Tasks = ({ projectData }: Props) => {
                             ID PL-{task.id}
                           </p>
                           <p className="text-sm font-semibold leading-6">
-                            {task.title}
+                            {task.name}
                           </p>
                         </div>
                       </div>
                       <div className="flex shrink-0 items-center gap-x-4">
-                        <FlatBadge />
-                        <p className="text-gray-400">
-                          <ChevronRightIcon className="h-5 w-5" />
-                        </p>
+                        <AvatarGroup />
+                        {/* Other components */}
                       </div>
                     </SelectorList>
                   ))}
-                </Fragment>
+                </React.Fragment>
               ))}
             </ul>
           </section>
 
-          <div>
-            {/* <Details
-              id={tasks[taskIndex].id}
-              title={tasks[taskIndex].title}
-            /> */}
-          </div>
+          <section aria-labelledby="projects">
+            <h2 className="sr-only" id="projects">
+              All Projects
+            </h2>
+            <div className="flex justify-between">
+              <h2 className="text-xl pb-4">Task Details</h2>
+              <ListMenu />
+            </div>
+            <ul className="w-2/3">
+              {projects.length > 0 &&
+              projectIndex < projects.length &&
+              projects[projectIndex].tasksList &&
+              taskIndex < projects[projectIndex].tasksList.length ? (
+                <ul className="w-2/3">
+                  <li className="flex flex-wrap justify-between py-2">
+                    <h4>Name</h4>
+                    <p>{projects[projectIndex].tasksList[taskIndex].name}</p>
+                  </li>
+                  <li className="flex flex-wrap justify-between py-2">
+                    <h4>Status</h4>
+                    <p>{projects[projectIndex].tasksList[taskIndex].status}</p>
+                  </li>
+                  <li className="flex flex-wrap justify-between py-2">
+                    <h4>Priority</h4>
+                    <p>
+                      <FlatBadge />
+                      {/* {projects[projectIndex].tasksList[taskIndex].priority} */}
+                    </p>
+                  </li>
+                  <li className="flex flex-wrap justify-between py-2">
+                    <h4>Asignee</h4>
+                    <div className="flex gap-3">
+                      <img
+                        className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                      <p>Jeniffer S</p>
+                    </div>
+                  </li>
+                  <li className="flex flex-wrap justify-between py-2">
+                    <h4>Start date</h4>
+                    <p>
+                      {projects[projectIndex].tasksList[taskIndex].startDate}
+                    </p>
+                  </li>
+                  <li className="flex flex-wrap justify-between py-2">
+                    <h4>Target date</h4>
+                    <p>
+                      {projects[projectIndex].tasksList[taskIndex].targetDate}
+                    </p>
+                  </li>
+                  <li className="flex flex-wrap justify-between py-2">
+                    <h4>Project</h4>
+                    <p>{projects[projectIndex].client}</p>
+                  </li>
+                </ul>
+              ) : (
+                <li>No task details available</li>
+              )}
+            </ul>
+          </section>
         </TwoColumns>
       </Main>
     </SplitScreen>
