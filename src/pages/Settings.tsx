2abbc5fb32f9c8bc63866baa6419332a6d65c650
Johnny Estrada <johnnye562@yahoo.com>
2023-11-20 14:43:32 -0800

    Update padding top on settings page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -15,7 +15,7 @@ function Projects() {
           <></>
         </Header>
 
-        <div className="space-y-10 divide-y divide-gray-900/10">
+        <div className="space-y-10 divide-y divide-gray-900/10 pt-10">
           <div className="grid grid-cols-1 gap-x-8 gap-y-8 lg:grid-cols-3">
             <div className="px-4 sm:px-0">
               <h2 className="text-base font-semibold leading-7 text-gray-900">
