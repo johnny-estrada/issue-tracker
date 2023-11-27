184e5ae46ab5d1dd81661b0ed18e0c216e4da513
Johnny Estrada <johnnye562@yahoo.com>
2023-11-27 15:01:18 -0800

    Format server file

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const { Sequelize } = require('sequelize');
+import express from "express";
+import cors from "cors";
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
-const uri = process.env.MYSQL_URI
+const uri = process.env.MYSQL_URI;
 const port = process.env.PORT || 5001;
 
 app.use(cors());
@@ -14,15 +15,15 @@ app.use(express.json());
 const sequelize = new Sequelize(uri, {});
 
 async function initialize() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-        app.listen(port, () => {
-            console.log(`Server is running on port: ${port}`);
-        });
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
 
 initialize();
