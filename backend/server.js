f7dfcd1bd161bb0b0cfbccc0a812852cdfdf41a8
Johnny Estrada <johnnye562@yahoo.com>
2023-11-27 13:16:27 -0800

    Create server and connect to db

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { Sequelize } = require('sequelize');
+
+require('dotenv').config();
+
+const app = express();
+const uri = process.env.MYSQL_URI
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+const sequelize = new Sequelize(uri, {});
+
+async function initialize() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+        app.listen(port, () => {
+            console.log(`Server is running on port: ${port}`);
+        });
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+initialize();
