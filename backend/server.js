623ddae488c3a61a266fd5dcb0270e67a3241f71
Johnny Estrada <johnnye562@yahoo.com>
2023-11-28 14:34:56 -0800

    Remove db connection from server

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,10 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const uri = process.env.MYSQL_URI;
 const port = process.env.PORT || 5001;
-
-connectDB(uri);
 
 const app = express();
 
