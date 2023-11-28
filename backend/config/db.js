81d9248a14e40977ed7629d8b21ea44f5d3ec0e0
Johnny Estrada <johnnye562@yahoo.com>
2023-11-28 00:00:37 -0800

    Create db connection

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+
+
+const connectDB = async function initialize(uri) {
+    const sequelize = new Sequelize(uri, {});
+    
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+}
+
+export default connectDB;
