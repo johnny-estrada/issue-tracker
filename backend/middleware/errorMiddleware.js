988b116577bb3d8a6bff548bbd3a55697bd652eb
Johnny Estrada <johnnye562@yahoo.com>
2023-11-27 22:47:33 -0800

    Format errorMiddleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,22 +1,22 @@
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`)
-    res.status(404)
-    next(error)
-}
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
 
 const errorHandler = (err, req, res, next) => {
-    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    let message = err.message;
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
 
-    if (err.name === 'CastError' && err.kind === 'objectId') {
-        statusCode = 404;
-        message = 'Resource not found'
-    }
+  if (err.name === "CastError" && err.kind === "objectId") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
 
-    res.status(statusCode).json({
-        message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
-    })
-}
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
 
-export { notFound, errorHandler }
+export { notFound, errorHandler };
