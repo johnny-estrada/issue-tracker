460764ebdcc9cc801c83e2070fa4bd1c8d4c7de5
Johnny Estrada <johnnye562@yahoo.com>
2023-11-28 00:01:00 -0800

    Create userController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,45 @@
+// Allows use async await without wrapping everything in try catch
+import asyncHandler from "express-async-handler";
+
+// @desc Auth user/set token
+// route POST /api/users/auth
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Auth User" });
+});
+
+// @desc Register a new user
+// route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc Logout user
+// route POST /api/users/logout
+// @access Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc Get user profile
+// route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User profile" });
+});
+
+// @desc Update user profile
+// route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update user profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
