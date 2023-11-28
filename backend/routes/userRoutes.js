ade3f846f1bff251ffcf02756ccfb9c98d148f35
Johnny Estrada <johnnye562@yahoo.com>
2023-11-28 00:01:34 -0800

    Create userRoutes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
+
+router.post("/", registerUser);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.get("/profile", getUserProfile);
+router.put("/profile", updateUserProfile);
+
+export default router;
