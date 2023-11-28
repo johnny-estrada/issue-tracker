8e9b7e53c59a10822f4fc8276a2b93640b48312b
Johnny Estrada <johnnye562@yahoo.com>
2023-11-27 23:55:21 -0800

    Add db connection and custom error handlers to server

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,30 @@
 import express from "express";
 import cors from "cors";
-import { Sequelize } from "sequelize";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from './config/db.js'
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const app = express();
 const uri = process.env.MYSQL_URI;
 const port = process.env.PORT || 5001;
+
+connectDB(uri)
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const sequelize = new Sequelize(uri, {});
+app.use("/api/users", userRoutes);
 
-async function initialize() {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    app.listen(port, () => {
-      console.log(`Server is running on port: ${port}`);
-    });
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-}
+app.get("/", (req, res) => res.send("Server is ready"));
 
-initialize();
+// Custom error handler
+app.use(notFound);
+app.use(errorHandler);
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
