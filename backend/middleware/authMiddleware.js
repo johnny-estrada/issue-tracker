971741f9405185b818701b1187417dc5ba97b75e
Johnny Estrada <johnnye562@yahoo.com>
2023-11-29 18:51:41 -0800

    Update authMiddleware to use user id

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,8 +10,9 @@ const protect = asyncHandler(async (req, res, next) => {
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      req.user = await User.findById(decoded.userId).select("-password");
+      req.user = await User.findByPk(decoded.userId, {
+        attributes: { exclude: ["password"] },
+      });
 
       next();
     } catch (error) {
