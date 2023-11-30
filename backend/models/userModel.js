b4486f02933722d505c0e4d22de8ff9860693f59
Johnny Estrada <johnnye562@yahoo.com>
2023-11-29 19:01:47 -0800

    Add bcrypt logic to userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,28 +1,54 @@
-import { DataTypes } from 'sequelize'
-import sequelize from '../config/db.js'
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+import bcrypt from "bcrypt";
 
-const User = sequelize.define('User', {
+const User = sequelize.define(
+  "User",
+  {
     name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-}, {
-    timestamps: true
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+User.beforeCreate(async (user) => {
+  try {
+    const hashedPassword = await bcrypt.hash(user.password, 12);
+    user.password = hashedPassword;
+  } catch (error) {
+    throw new Error("Error hashing the password.", error);
+  }
 });
 
-User.sync().then(() => {
-    console.log('User table created successfully!');
- }).catch((error) => {
-    console.error('Unable to create table : ', error);
- });
+// This hook checks if the password field has changed before hashing it again
+User.beforeUpdate(async (user) => {
+  if (user.changed("password")) {
+    const hashedPassword = await bcrypt.hash(user.password, 12);
+    user.password = hashedPassword;
+  }
+});
 
- export default User;
+User.prototype.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// User.sync().then(() => {
+//     console.log('User table created successfully!');
+// }).catch((error) => {
+//     console.error('Unable to create table : ', error);
+// });
+
+export default User;
