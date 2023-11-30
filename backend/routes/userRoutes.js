f49c12c9b97be5cab6fee4ae808daaeabfd79aec
Johnny Estrada <johnnye562@yahoo.com>
2023-11-29 16:03:18 -0800

    Add protected routes to userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,11 +7,12 @@ import {
   getUserProfile,
   updateUserProfile,
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.get("/profile", getUserProfile);
-router.put("/profile", updateUserProfile);
+router.get("/profile", protect, getUserProfile);
+router.put("/profile", protect, updateUserProfile);
 
 export default router;
