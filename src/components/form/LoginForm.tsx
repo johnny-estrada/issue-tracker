e2242819818144e11bcf4c2e627ebf5da0af1605
Johnny Estrada <johnnye562@yahoo.com>
2023-11-29 23:07:02 -0800

    Create LoginForm

---
+++ src/components/form/LoginForm.tsx
@@ -0,0 +1,91 @@
+const LoginForm = (props) => {
+  return (
+    <div>
+      <form
+        action="#"
+        method="POST"
+        className="space-y-6"
+        onSubmit={props.submitHandler}
+      >
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              value={props.email}
+              onChange={(e) => props.setEmail(e.target.value)}
+              required
+              className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Password
+          </label>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              value={props.password}
+              onChange={(e) => props.setPassword(e.target.value)}
+              required
+              className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+            />
+            <label
+              htmlFor="remember-me"
+              className="ml-3 block text-sm leading-6 text-gray-700"
+            >
+              Remember me
+            </label>
+          </div>
+
+          <div className="text-sm leading-6">
+            <a
+              href="#"
+              className="font-semibold text-orange-500 hover:text-orange-400"
+            >
+              Forgot password?
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
