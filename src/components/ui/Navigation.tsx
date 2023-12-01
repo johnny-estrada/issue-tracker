0060c7d59ffcd708fb537b423626b12ebeab56f2
Johnny Estrada <johnnye562@yahoo.com>
2023-11-30 20:43:14 -0800

    Update Navigation icon color when active

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -22,15 +22,13 @@ export default function Navigation({ navigation }: Props) {
                   isPending
                     ? "pending"
                     : isActive
-                    ? "lg:bg-gray-600 text-white hover:text-white hover:bg-orange-500 group lg:flex lg:w-full text-xs lg:text-sm leading-6 font-semibold p-4 pl-8 pointer-events-none"
-                    : "text-gray-400 hover:text-white hover:bg-orange-500 group lg:flex lg:w-full text-xs lg:text-sm leading-6 font-semibold p-4 pl-8"
+                      ? "lg:bg-gray-600 text-white hover:text-white hover:bg-orange-500 group lg:flex lg:w-full text-xs lg:text-sm leading-6 font-semibold p-4 pl-8 pointer-events-none"
+                      : "text-gray-400 hover:text-white hover:bg-orange-500 group lg:flex lg:w-full text-xs lg:text-sm leading-6 font-semibold p-4 pl-8"
                 }
               >
                 <item.icon
                   className={classNames(
-                    item.current
-                      ? "text-white"
-                      : "text-gray-400 group-hover:text-white",
+                    item.current ? "" : "text-gray-400 group-hover:text-white",
                     "h-6 w-6 shrink-0 m-auto lg:mr-4 lg:ml-0",
                   )}
                 />
