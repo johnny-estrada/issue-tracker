381bb63e18f420b7492a5641a7c694f44551622f
Johnny Estrada <johnnye562@yahoo.com>
2023-11-30 20:41:01 -0800

    Add provider, store and toastify to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import store from "./redux/store";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Tasks from "./pages/Tasks";
@@ -14,33 +18,36 @@ import SignUp from "./pages/SignUp";
 import { projects, tasks, lineChart, barChart } from "./data/index";
 
 function App() {
-  const [user, setUser] = useState({ name: "Johnny", isAdmin: true });
+  // const [user, setUser] = useState({ name: "Johnny", isAdmin: true });
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          index={true}
-          path="/"
-          element={
-            <Dashboard
-              projectData={projects}
-              taskData={tasks}
-              lineChartData={lineChart}
-              barChartData={barChart}
-            />
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route
-          path="/projects"
-          element={<Projects projectData={projects} taskData={tasks} />}
-        />
-        <Route path="/tasks" element={<Tasks projectData={projects} />} />
-        <Route path="/notifications" element={<Notifications />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
+      <Provider store={store}>
+        <ToastContainer />
+        <Routes>
+          <Route
+            index={true}
+            path="/"
+            element={
+              <Dashboard
+                projectData={projects}
+                taskData={tasks}
+                lineChartData={lineChart}
+                barChartData={barChart}
+              />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/projects"
+            element={<Projects projectData={projects} taskData={tasks} />}
+          />
+          <Route path="/tasks" element={<Tasks projectData={projects} />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
