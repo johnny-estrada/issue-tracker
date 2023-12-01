9a6cbd95a4abb18c12b6e590a6cd74533d7eae7c
Johnny Estrada <johnnye562@yahoo.com>
2023-11-30 18:11:45 -0800

    Update logo path in Logo component

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -3,7 +3,7 @@ export default function Logo() {
     <div className="hidden lg:flex shrink-0 items-center gap-4 text-white font-semibold text-2xl">
       <img
         className="hidden h-8 w-auto lg:inline"
-        src="/logo.svg"
+        src="/icons/logo.svg"
         alt="Three, different orange hue colored vertical lines."
       />
       Kanban
