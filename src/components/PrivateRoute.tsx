327019aa8c9033a4a9b5fe6b4a8a996cf176d6c9
Johnny Estrada <johnnye562@yahoo.com>
2023-11-30 21:51:12 -0800

    Format PrivateRoute component

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,10 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom";
-import {useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
 
 const PrivateRoute = () => {
-    const {userInfo} = useSelector((state) => state.auth);
+  const { userInfo } = useSelector((state) => state.auth);
 
-  return userInfo ? <Outlet /> : <Navigate to='/login' replace />
-}
+  return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
+};
 
-export default PrivateRoute
+export default PrivateRoute;
