c90006c2d14bdf20daeeab9332b614a649531046
Johnny Estrada <johnnye562@yahoo.com>
2023-11-30 20:08:41 -0800

    Format Loader component

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,7 +1,13 @@
 const Loader = () => {
   return (
-    <div><img className="block w-28 h-28 m-auto" src='/images/animated-spinner.svg' alt="" /></div>
-  )
-}
+    <div>
+      <img
+        className="block w-28 h-28 m-auto"
+        src="/images/animated-spinner.svg"
+        alt=""
+      />
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
