6f651f29ee68819a48dece634c77806a0622fb6b
Johnny Estrada <johnnye562@yahoo.com>
2023-11-30 18:15:45 -0800

    Update Loader image path

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div><img className="block w-28 h-28 m-auto" src='/animated-spinner.svg' alt="" /></div>
+    <div><img className="block w-28 h-28 m-auto" src='/images/animated-spinner.svg' alt="" /></div>
   )
 }
 
