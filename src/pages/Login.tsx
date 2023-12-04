7fabd828a460b852dde32bc640886642432358dd
Johnny Estrada <johnnye562@yahoo.com>
2023-12-04 15:41:19 -0800

    Add Login functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,50 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import LoginForm from "../components/form/LoginForm";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useLoginMutation } from "../slices/usersApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { toast } from "react-toastify";
+import Loader from "../components/ui/Loader";
+// import LoginForm from "../components/form/LoginForm";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const customId = "custom-id-yes";
+
+  const notify = () => {
+    if (!toast.isActive(customId)) {
+      toast({
+        toastId: customId,
+      });
+    }
+  };
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userInfo]);
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("submit");
+    try {
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate("/");
+    } catch (err) {
+      toast.error(err?.data?.message || err.error, {
+        toastId: customId,
+      });
+    }
   };
 
   return (
@@ -19,7 +55,7 @@ export default function Login() {
             <div className="">
               <img
                 className="h-10 w-auto"
-                src="./logo.svg"
+                src="./icons/logo.svg"
                 alt="Your Company"
               />
               <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -37,7 +73,94 @@ export default function Login() {
             </div>
 
             <div className="mt-10">
-              <LoginForm onSubmit={submitHandler} />
+              <div>
+                <form
+                  method="POST"
+                  className="space-y-6"
+                  onSubmit={submitHandler}
+                >
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Email address
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 px-3 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <input
+                        id="remember-me"
+                        name="remember-me"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                      <label
+                        htmlFor="remember-me"
+                        className="ml-3 block text-sm leading-6 text-gray-700"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+
+                    <div className="text-sm leading-6">
+                      <a
+                        href="#"
+                        className="font-semibold text-orange-500 hover:text-orange-400"
+                      >
+                        Forgot password?
+                      </a>
+                    </div>
+                  </div>
+
+                  <div>
+                    {isLoading && <Loader />}
+
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Sign in
+                    </button>
+                  </div>
+                </form>
+              </div>
 
               <div className="mt-10">
                 <div className="relative">
