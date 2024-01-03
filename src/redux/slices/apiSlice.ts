b28f2f379c45b842ea5d8c27547f2cffb70725f1
Johnny Estrada <johnnye562@yahoo.com>
2024-01-03 00:00:27 -0800

    Update tagTypes in apiSlice

--- src/redux/slices/apiSlice.ts
+++ src/redux/slices/apiSlice.ts
@@ -4,6 +4,6 @@ const baseQuery = fetchBaseQuery({ baseUrl: "" });
 
 export const apiSlice = createApi({
   baseQuery,
-  tagTypes: ["User"],
+  tagTypes: ["User", "Project", "Tasks"],
   endpoints: (builder) => ({}),
 });
