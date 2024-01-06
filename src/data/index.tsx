2a7ac3164a397be2b37625d69bd60f4e2a19f0e4
Johnny Estrada <johnnye562@yahoo.com>
2024-01-05 21:44:46 -0800

    Add people data to data file

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -87,6 +87,19 @@ export const barChart = [
   },
 ];
 
+export const people = [
+  {
+    name: "Lindsay Walton",
+    title: "Front-end Developer",
+    department: "Optimization",
+    email: "lindsay.walton@example.com",
+    role: "Member",
+    image:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  // More people...
+];
+
 export const users = [
   {
     firstName: "Johnny",
