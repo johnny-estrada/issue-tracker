e88895ba424dd91ed30bf9a077cb7ee437acc521
Johnny Estrada <johnnye562@yahoo.com>
2024-01-05 22:04:58 -0800

    Add date formatting to Details

--- src/components/ui/Details.tsx
+++ src/components/ui/Details.tsx
@@ -1,30 +1,38 @@
-import ListMenu from "../ui/ListMenu";
+import { useState, useEffect } from "react";
 import AvatarGroup from "./AvatarGroup";
+import { formatDate } from "../../utils/formatting";
 
-const Details = (props) => {
+const Details = ({ description, client, startDate, targetDate }) => {
+  const [formattedDate1, setFormattedDate1] = useState("");
+  const [formattedDate2, setFormattedDate2] = useState("");
+
+  useEffect(() => {
+    const date1 = startDate;
+    const options1 = { year: "numeric", month: "short", day: "numeric" };
+    const formatted1 = formatDate(date1, options1);
+    setFormattedDate1(formatted1);
+
+    const date2 = targetDate;
+    const options2 = { year: "numeric", month: "short", day: "numeric" };
+    const formatted2 = formatDate(date2, options2);
+    setFormattedDate2(formatted2);
+  }, [startDate, targetDate]);
+
   return (
     <section aria-labelledby="projects">
-      <h2 className="sr-only" id="projects">
-        All Projects
-      </h2>
-      <div className="flex  justify-between">
-        <h2 className="text-xl pb-4">{props.title}</h2>
-        <ListMenu />
-      </div>
-      <p className="pb-2">{props.description}</p>
-
+      <p className="pb-2">{description}</p>
       <ul className="w-2/3">
         <li className="flex flex-wrap justify-between py-2">
           <h4>Client</h4>
-          <p>{props.client}</p>
+          <p>{client}</p>
         </li>
         <li className="flex flex-wrap justify-between py-2">
           <h4>Start date</h4>
-          <p>{props.startDate}</p>
+          <p>{formattedDate1}</p>
         </li>
         <li className="flex flex-wrap justify-between py-2">
           <h4>Target date</h4>
-          <p>{props.targetDate}</p>
+          <p>{formattedDate2}</p>
         </li>
         <li className="flex flex-wrap justify-between py-2">
           <h4>Members</h4>
