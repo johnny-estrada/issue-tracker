002e7df27151f2321627aded9046209a1ce1a406
Johnny Estrada <johnnye562@yahoo.com>
2024-01-05 22:05:38 -0800

    Minor text update to ButtonGroup

--- src/components/ui/ButtonGroup.tsx
+++ src/components/ui/ButtonGroup.tsx
@@ -23,7 +23,7 @@ export default function ButtonGroup() {
         type="button"
         className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
       >
-        arhived
+        on hold
       </button>
     </span>
   );
