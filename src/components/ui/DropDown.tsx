eca17bec47e95abbbdd7b983dc66e87373122406
Johnny Estrada <johnnye562@yahoo.com>
2024-01-07 23:35:23 -0800

    Update state slices imports

--- src/components/ui/DropDown.tsx
+++ src/components/ui/DropDown.tsx
@@ -4,8 +4,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useLogoutMutation } from "../../redux/slices/usersApiSlice";
-import { logout } from "../../redux/slices/authSlice";
+import { useLogoutMutation } from "../../services/state/redux/slices/usersApiSlice";
+import { logout } from "../../services/state/redux/slices/authSlice";
 import { UserCircleIcon } from "@heroicons/react/24/solid";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
