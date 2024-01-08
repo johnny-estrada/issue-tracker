b044927027b3bcad247aa3ace16a16e5559e12de
Johnny Estrada <johnnye562@yahoo.com>
2024-01-07 18:06:17 -0800

    Add getUsers to userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,13 +6,15 @@ import {
   logoutUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
 router.get("/profile", protect, getUserProfile);
 router.put("/settings", protect, updateUserProfile);
+router.get('/', protect, admin, getUsers)
 
 export default router;
