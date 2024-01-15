d07cb512ee78c9d353521b0aff9db5e2a2f4e8f0
Johnny Estrada <johnnye562@yahoo.com>
2024-01-15 01:32:24 -0800

    Add morgan and file path to morgan.log

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ import taskRoutes from "./routes/taskRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
+
 
 dotenv.config();
 
@@ -18,6 +22,13 @@ app.use(express.json());
 // Allows us to send form data
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Create a write stream for the log file in the 'logs' directory
+const logFilePath = path.join(process.cwd(), "logs", "morgan.log");
+const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
+
+// Morgan middleware for logging to a file
+app.use(morgan("combined", { stream: logStream }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
