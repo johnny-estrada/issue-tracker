65fcd267fbc5b5b3aa1cf1b4fa2c7ab23db0909a
Johnny Estrada <johnnye562@yahoo.com>
2024-01-15 11:23:02 -0800

    Create tasksApiSlice

---
+++ src/services/state/redux/slices/tasksApiSlice.ts
@@ -0,0 +1,48 @@
+import { apiSlice } from "./apiSlice";
+
+const TASK_URL = "/api/tasks";
+
+export const tasksApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getTask: builder.query({
+      query: () => ({
+        url: `${TASK_URL}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getTaskDetails: builder.query({
+      query: (taskId) => ({
+        url: `${TASK_URL}/${taskId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    createTask: builder.mutation({
+      query: (data) => ({
+        url: `${TASK_URL}`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Tasks"],
+    }),
+    updateProject: builder.mutation({
+      query: (data) => ({
+        url: `${TASK_URL}/${data.taskId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Tasks"],
+    }),
+    deleteProject: builder.mutation({
+      query: (taskId) => ({
+        url: `${TASK_URL}/${taskId}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetTaskQuery,
+  useGetTaskDetailsQuery,
+  useCreateTaskMutation,
+} = tasksApiSlice;
