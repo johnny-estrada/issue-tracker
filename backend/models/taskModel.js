ed24be8e95b531aff87d58bdc442a0a5d0f01065
Johnny Estrada <johnnye562@yahoo.com>
2024-01-15 11:17:54 -0800

    Add data types and associations to taskModel

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -1,27 +1,69 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
+import Project from "./projectModel.js";
+import User from "./userModel.js";
 
-const Task = sequelize.define("Task", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
+const Task = sequelize.define(
+  "Task",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM,
+      values: ["to do", "in progress", "backlog", "done"],
+      defaultValue: "to do",
+    },
+    priority: {
+      type: DataTypes.ENUM,
+      values: ["low", "medium", "high"],
+      defaultValue: "low",
+    },
+    asignee: {
+      type: DataTypes.STRING,
+    },
+    startDate: {
+      type: DataTypes.DATE,
+    },
+    targetDate: {
+      type: DataTypes.DATE,
+    },
+    project: {
+      type: DataTypes.STRING,
+    },
+    attachments: {
+      type: DataTypes.JSON,
+    },
+    details: {
+      type: DataTypes.TEXT,
+    },
+    subtask: {
+      type: DataTypes.JSON,
+    },
+    activity: {
+      type: DataTypes.JSON,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    projectId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
   },
-  details: {
-    type: DataTypes.TEXT,
-  },
-  startDate: {
-    type: DataTypes.STRING,
-  },
-  targetDate: {
-    type: DataTypes.STRING,
-  },
-  priority: {
-    type: DataTypes.STRING,
-  },
-  status: {
-    type: DataTypes.STRING,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const setupAssociations = () => {
+  Task.belongsTo(User, { foreignKey: "userId" });
+  Task.belongsTo(Project, { foreignKey: "projectId" });
+};
+
+export { setupAssociations };
 
 Task.sync()
   .then(() => {
