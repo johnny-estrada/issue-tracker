8e4a9266048551d62bf3d5f3fc51d816817d80b2
Johnny Estrada <johnnye562@yahoo.com>
2024-01-15 11:19:40 -0800

    Create taskRoutes

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+import {
+  createTask,
+  deleteTask,
+  getTaskById,
+  getTasks,
+  updateTask,
+} from "../controllers/taskController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").get(protect, getTasks).post(protect, admin, createTask);
+router
+  .route("/:id")
+  .get(protect, getTaskById)
+  .put(protect, updateTask)
+  .delete(protect, deleteTask);
+
+export default router;
