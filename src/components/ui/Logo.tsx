189842faee0690f63875ecb9f410df7f00a61800
Johnny Estrada <johnnye562@yahoo.com>
2024-01-16 00:22:11 -0800

    Update Logo image to svg

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,11 +1,13 @@
 export default function Logo() {
   return (
-    <div className="hidden lg:flex shrink-0 items-center gap-4 text-white font-semibold text-2xl">
-      <img
-        className="hidden h-8 w-auto lg:inline"
-        src="/icons/logo.svg"
-        alt="Three, different orange hue colored vertical lines."
-      />
+    <div className="hidden lg:flex shrink-0 items-center gap-3 text-white font-semibold text-2xl">
+      <svg width="24" height="25" xmlns="http://www.w3.org/2000/svg">
+        <g fill="#FF7A50" fillRule="evenodd">
+          <rect width="6" height="25" rx="2" />
+          <rect opacity=".75" x="9" width="6" height="25" rx="2" />
+          <rect opacity=".5" x="18" width="6" height="25" rx="2" />
+        </g>
+      </svg>
       Kanban
     </div>
   );
