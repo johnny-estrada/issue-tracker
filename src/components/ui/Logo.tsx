f49f2d2f2fc80a26e81bef89e0f14406004fdf99
Johnny Estrada <johnnye562@yahoo.com>
2024-01-18 23:49:48 -0800

    Add home link to Logo

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,14 +1,18 @@
+import { NavLink } from "react-router-dom";
+
 export default function Logo() {
   return (
-    <div className="hidden lg:flex shrink-0 items-center gap-3 text-white font-semibold text-2xl">
-      <svg width="24" height="25" xmlns="http://www.w3.org/2000/svg">
-        <g fill="#FF7A50" fillRule="evenodd">
-          <rect width="6" height="25" rx="2" />
-          <rect opacity=".75" x="9" width="6" height="25" rx="2" />
-          <rect opacity=".5" x="18" width="6" height="25" rx="2" />
-        </g>
-      </svg>
-      Kanban
-    </div>
+    <NavLink to={"/"}>
+      <div className="hidden lg:flex shrink-0 items-center gap-3 text-white font-semibold text-2xl">
+        <svg width="24" height="25" xmlns="http://www.w3.org/2000/svg">
+          <g fill="#FF7A50" fillRule="evenodd">
+            <rect width="6" height="25" rx="2" />
+            <rect opacity=".75" x="9" width="6" height="25" rx="2" />
+            <rect opacity=".5" x="18" width="6" height="25" rx="2" />
+          </g>
+        </svg>
+        Kanban
+      </div>
+    </NavLink>
   );
 }
