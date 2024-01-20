ece04eb6883f3e69504f5bf6d5b07e5549fe62aa
Johnny Estrada <johnnye562@yahoo.com>
2024-01-20 00:14:02 -0800

    Update Details css layout

--- src/components/ui/Details.tsx
+++ src/components/ui/Details.tsx
@@ -24,19 +24,19 @@ const Details = ({ description, client, startDate, targetDate, members }) => {
     <section aria-labelledby="projects">
       <p className="pb-2">{description}</p>
       <ul className="w-2/3">
-        <li className="flex flex-wrap justify-between py-2">
+        <li className="grid grid-cols-2 py-2">
           <h4>Client</h4>
           <p>{client}</p>
         </li>
-        <li className="flex flex-wrap justify-between py-2">
+        <li className="grid grid-cols-2 py-2">
           <h4>Start date</h4>
           <p>{formattedDate1}</p>
         </li>
-        <li className="flex flex-wrap justify-between py-2">
+        <li className="grid grid-cols-2 py-2">
           <h4>Target date</h4>
           <p>{formattedDate2}</p>
         </li>
-        <li className="flex flex-wrap justify-between py-2">
+        <li className="grid grid-cols-2 py-2">
           <h4>Members</h4>
           <div className="flex -space-x-2 overflow-hidden p-1">
             {members ? (
