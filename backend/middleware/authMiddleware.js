dccc0643f1c7a45fff8e096f98ce8fc9ad08f05b
Johnny Estrada <johnnye562@yahoo.com>
2024-01-19 16:03:02 -0800

    Minor update to authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -27,10 +27,10 @@ const protect = asyncHandler(async (req, res, next) => {
 
 // Add admin middleware here
 const admin = asyncHandler(async (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
+  if (req.user && req.user.role === "admin") {
     next();
   } else {
-    res.status(403); // 403 Forbidden
+    res.status(403);
     throw new Error("Not authorized as an admin");
   }
 });
