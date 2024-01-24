2659442fde4ef5d959de4694e5cd5f28a90d2306
Johnny Estrada <johnnye562@yahoo.com>
2024-01-24 11:07:52 -0800

    Add additional routes to userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,13 +7,23 @@ import {
   getUserProfile,
   updateUserProfile,
   getUsers,
+  getUserById,
+  updateUser,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
+
 router.get("/profile", protect, getUserProfile);
+
 router.put("/settings", protect, updateUserProfile);
+
+// router.put("/profile/:id", protect, deleteUserProfile);
+
+router.get("/:id", protect, getUserById);
+router.put("/:id", protect, updateUser);
+// router.put("/:id", protect, deleteUser);
 
 export default router;
