097c1922b66adb4d9961594cbcb29388e6125730
Johnny Estrada <johnnye562@yahoo.com>
2024-01-27 17:30:25 -0800

    Move store file to redux folder

--- src/services/state/redux/store.ts
+++ src/services/state/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../services/state/redux/slices/authSlice";
-import { apiSlice } from "../services/state/redux/slices/apiSlice";
+import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
   reducer: {
