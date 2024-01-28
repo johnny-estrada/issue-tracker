cf08097b6e050ee3505138156b839aa02d256565
Johnny Estrada <johnnye562@yahoo.com>
2024-01-28 01:17:10 -0800

    Move Logo file to header component folder

--- src/components/sidebar/Logo.tsx
+++ src/components/sidebar/Logo.tsx
@@ -3,15 +3,15 @@ import { NavLink } from "react-router-dom";
 export default function Logo() {
   return (
     <NavLink to={"/"}>
-      <div className="hidden lg:flex shrink-0 items-center gap-3 text-white font-semibold text-2xl">
-        <svg width="24" height="25" xmlns="http://www.w3.org/2000/svg">
+      <div className="hidden lg:flex shrink-0 items-center gap-3">
+        <svg width="26" height="26" xmlns="http://www.w3.org/2000/svg">
           <g fill="#FF7A50" fillRule="evenodd">
             <rect width="6" height="25" rx="2" />
             <rect opacity=".75" x="9" width="6" height="25" rx="2" />
             <rect opacity=".5" x="18" width="6" height="25" rx="2" />
           </g>
         </svg>
-        Kanban
+        <span className="text-white text-xl">Kanban</span>
       </div>
     </NavLink>
   );
