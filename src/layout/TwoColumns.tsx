7f78f3a0f41e8585fbbf3f3352cae9d35f3d67e8
Johnny Estrada <johnnye562@yahoo.com>
2024-02-02 00:39:35 -0800

    Minor update to TwoColumns

--- src/layout/TwoColumns.tsx
+++ src/layout/TwoColumns.tsx
@@ -8,17 +8,17 @@ const Layout = ({ children }) => {
   const title = "Notifications";
   return (
     <>
+      <Sidebar />
       <div className="grid grid-cols-1 overflow-hidden lg:flex flex-col lg:h-screen lg:ml-[288px]">
-        <Sidebar />
         <Header>
           {headerTop}
           {headerBottom}
         </Header>
         <main className="lg:flex lg:h-screen">
-          <div className="lg:block lg:flex-1 lg:px-14 lg:pt-10 lg:mb-auto">
+          <div className="lg:flex-1 lg:px-14 lg:pt-10 lg:mb-auto">
             {mainLeft}
           </div>
-          <div className="lg:block lg:flex-1 lg:px-14 lg:pt-10 border-l border-gray-200 hidden">
+          <div className="lg:flex-1 lg:px-14 lg:pt-10 border-l border-gray-200 lg:block hidden">
             {mainRight}
           </div>
         </main>
