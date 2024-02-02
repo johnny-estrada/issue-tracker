433f51aca03ff37e1cc6896682509c3e7c8c2888
Johnny Estrada <johnnye562@yahoo.com>
2024-02-02 15:13:30 -0800

    Create TwoColumnsWide Layout

---
+++ src/layout/TwoColumnsWide.tsx
@@ -0,0 +1,30 @@
+import Header from "../components/header/Header";
+
+import Sidebar from "../components/sidebar/Sidebar";
+
+const TwoColumnsWide = ({ children }) => {
+  const [headerTop, headerBottom, mainRight, mainLeftTop, mainLeftBottom] =
+    children;
+
+  return (
+    <>
+      <Sidebar />
+      <div className="grid grid-cols-1 lg:flex flex-col lg:h-screen lg:ml-[288px]">
+        <Header>
+          {headerTop}
+          {headerBottom}
+        </Header>
+        {/* <Tabs data={lineChart} projectData={projects} /> */}
+        <main className="lg:flex lg:h-screen gap-10 lg:px-14 lg:pt-10 hidden">
+          <div className="w-1/3 mb-auto">{mainRight}</div>
+          <div className="flex-1 mb-auto">
+            {mainLeftTop}
+            {mainLeftBottom}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default TwoColumnsWide;
