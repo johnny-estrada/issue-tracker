d83a17efcbe296c0c72a242d733c9c76a8a3c94e
Johnny Estrada <johnnye562@yahoo.com>
2024-02-02 19:24:44 -0800

    Run format on Dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,15 +2,13 @@ import { useState, useEffect } from "react";
 import { useGetProjectsQuery } from "../../services/state/redux/slices/projectsApiSlice";
 import { useGetTaskQuery } from "../../services/state/redux/slices/tasksApiSlice";
 import { formatDate } from "../../utils/formatting";
-import Header from "../../components/header/Header";
 import DataDisplay from "./components/DataDisplay";
 import HeaderTitle from "../../components/header/HeaderTitle";
 import LineChart from "./components/LineChart";
 import BarCharts from "./components/BarCharts";
-import Sidebar from "../../components/sidebar/Sidebar";
-import Tabs from "../../components/ui/Tabs";
 import MyTask from "./components/MyTask";
 import MyProjects from "./components/MyProjects";
+import TwoColumnsWide from "../../layout/TwoColumnsWide";
 
 interface Props {
   lineChartData: object[];
@@ -64,56 +62,46 @@ const Dashboard = ({ lineChartData, barChartData }: Props) => {
 
   return (
     <>
-      <Sidebar />
-      <div className="grid grid-cols-1 lg:flex flex-col lg:h-screen lg:ml-[288px]">
-        <Header>
-          <HeaderTitle title={title} />
-          <DataDisplay projects={projects} />
-        </Header>
-        <Tabs data={lineChart} projectData={projects} />
-        <main className="lg:flex lg:h-screen gap-10 lg:px-14 lg:pt-10 hidden">
-          <div className="w-1/3 mb-auto">
-            <MyTask
-              id={tasks[projectIndex].id}
-              refetch={refetch}
-              projectIndex={projectIndex}
-              projects={projects}
-              tasks={tasks}
-              dates={formattedDates}
-            />
+      <TwoColumnsWide>
+        <HeaderTitle title={title} />
+        <DataDisplay projects={projects} />
+
+        <MyTask
+          id={tasks[projectIndex].id}
+          refetch={refetch}
+          projectIndex={projectIndex}
+          projects={projects}
+          tasks={tasks}
+          dates={formattedDates}
+        />
+
+        <section aria-labelledby="statistics">
+          <header>
+            <h2 className="sr-only" id="statistics">
+              Statistics
+            </h2>
+            <h2 className="text-2xl">Statistics</h2>
+            <p className="mb-5 text-sm  text-neutral-500">
+              tasks created vs tasks completed
+            </p>
+          </header>
+
+          <div className="flex">
+            <LineChart data={lineChart} />
+            <BarCharts data={barChart} />
           </div>
+        </section>
 
-          <div className="flex-1 mb-auto">
-            <section aria-labelledby="statistics">
-              <header>
-                <h2 className="sr-only" id="statistics">
-                  Statistics
-                </h2>
-                <h2 className="text-2xl">Statistics</h2>
-                <p className="mb-5 text-sm  text-neutral-500">
-                  tasks created vs tasks completed
-                </p>
-              </header>
-
-              <div className="flex">
-                <LineChart data={lineChart} />
-                <BarCharts data={barChart} />
-              </div>
-            </section>
-
-            <MyProjects
-              id={tasks[projectIndex].id}
-              refetch={refetch}
-              projectIndex={projectIndex}
-              projects={projects}
-              tasks={tasks}
-              dates={formattedDates}
-              toggleProjects={toggleProjects}
-              formattedDates={formattedDates}
-            />
-          </div>
-        </main>
-      </div>
+        <MyProjects
+          id={tasks[projectIndex].id}
+          refetch={refetch}
+          projectIndex={projectIndex}
+          projects={projects}
+          tasks={tasks}
+          toggleProjects={toggleProjects}
+          formattedDates={formattedDates}
+        />
+      </TwoColumnsWide>
     </>
   );
 };
