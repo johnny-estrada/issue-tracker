8c0a6ce08b0746d3db2564c2e22310d7a6bb89e5
Johnny Estrada <johnnye562@yahoo.com>
2024-02-02 16:18:46 -0800

    Add a route note to App

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
       <Provider store={store}>
         <ToastContainer />
         <Routes>
-           {/* Public Routes */}
+          {/* Public Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
 
