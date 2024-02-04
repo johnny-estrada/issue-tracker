d76341e34a83f1e4042cdec07048691bad80aa06
Johnny Estrada <johnnye562@yahoo.com>
2024-02-03 18:18:25 -0800

    Update css to make component mobile friendly

--- src/components/header/ButtonGroup.tsx
+++ src/components/header/ButtonGroup.tsx
@@ -11,15 +11,16 @@ export default function ButtonGroup({ titles }: Props) {
           <button
             key={i}
             type="button"
-            className="relative inline-flex items-center rounded-l-md bg-white px-3 
-            py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 
-            focus:z-10 shadow-sm"
+            className="relative inline-flex items-center rounded-l-md bg-white px-2.5 py-0 lg:px-3 
+            lg:py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 
+            focus:z-10 shadow-sm w-fit h-8"
           >
             {title}
           </button>
         </>
       ))}
 
+      <div className="hidden lg:block">
       <label
         htmlFor="sort"
         className="ml-10 mr-2 text-sm text-slate-500 my-auto"
@@ -29,13 +30,14 @@ export default function ButtonGroup({ titles }: Props) {
       <select
         id="sort"
         name="sort"
-        className="relative inline-flex items-center rounded-md bg-white px-3 text-sm font-normal text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        className="relative inline-flex items-center rounded-md bg-white px-3 text-sm font-normal text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 p-2"
       >
         <option value="status">status</option>
         <option value="priority">priority</option>
         <option value="start date">start date</option>
         <option value="target date">target date</option>
       </select>
+      </div>
     </span>
   );
 }
