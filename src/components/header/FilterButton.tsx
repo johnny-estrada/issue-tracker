eacd94854a00edfc74672da185f98ffba7639b1c
Johnny Estrada <johnnye562@yahoo.com>
2024-02-05 19:33:45 -0800

    Update FilterButton padding

--- src/components/header/FilterButton.tsx
+++ src/components/header/FilterButton.tsx
@@ -7,12 +7,13 @@ export default function FilterButton() {
       <button type="button" className="lg:hidden">
         <AdjustmentsVerticalIcon className="w-6 h-6 fill-stone-900" />
       </button>
+
       <button
         type="button"
-        className="hidden lg:block rounded-md bg-white px-3.5 py-2.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        className="hidden lg:flex justify-center align-middle mt-auto rounded-md bg-white px-3 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
       >
-        <div className="flex gap-1">
-          <FunnelIcon className="h-5 w-5" />
+        <div className="flex justify-center align-middle gap-1">
+          <FunnelIcon className="h-4 w-4" />
           <span className="text-sm">Filter</span>
         </div>
       </button>
