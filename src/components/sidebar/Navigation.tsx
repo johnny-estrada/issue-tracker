8dffe78dee25589314315c395b0b943488798c81
Johnny Estrada <johnnye562@yahoo.com>
2024-02-05 19:36:35 -0800

    Update Navigation layout for mobile

--- src/components/sidebar/Navigation.tsx
+++ src/components/sidebar/Navigation.tsx
@@ -13,17 +13,17 @@ export default function Navigation({ navigation }: Props) {
   return (
     <>
       <nav>
-        <ul className="flex justify-evenly lg:justify-between lg:flex-col">
+        <ul className="flex justify-center lg:justify-between lg:flex-col">
           {navigation.map((item) => (
-            <li key={item.name} className="flex">
+            <li key={item.name} className="flex mx-4 mt-2 mb-4  lg:m-0">
               <NavLink
                 to={item.href}
                 className={({ isActive, isPending }) =>
                   isPending
                     ? "pending"
                     : isActive
-                      ? "lg:bg-gray-600 text-white hover:text-white hover:bg-gray-700 group lg:flex lg:w-full text-xs lg:text-base leading-6 p-4 pl-8 pointer-events-none"
-                      : "text-neutral-400 hover:text-white hover:bg-gray-700 group lg:flex lg:w-full text-xs lg:text-base p-4 pl-8"
+                      ? "lg:bg-gray-600 text-white hover:text-white hover:bg-gray-700 group lg:flex lg:w-full text-xs lg:text-base leading-6 lg:p-4 lg:pl-8 pointer-events-none"
+                      : "text-neutral-400 hover:text-white hover:bg-gray-700 group lg:flex lg:w-full text-xs lg:text-base lg:p-4 lg:pl-8"
                 }
               >
                 <item.icon
