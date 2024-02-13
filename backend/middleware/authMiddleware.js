429a4450b31f2002a3358b595b2cc13cbe79c2ec
Johnny Estrada <johnnye562@yahoo.com>
2024-02-13 01:05:46 -0800

    Minor update to authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
-import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
