dfe79e7dd537f92031f221602f936ae1904266b2
Johnny Estrada <johnnye562@yahoo.com>
2024-02-20 20:35:13 -0800

    Add Breadcrumbs functionality

--- src/components/header/Breadcrumbs.tsx
+++ src/components/header/Breadcrumbs.tsx
@@ -1,12 +1,47 @@
-import { Link } from "react-router-dom";
-import { ChevronRightIcon, HomeIcon } from "@heroicons/react/20/solid";
-
-const pages = [{ name: "Projects", to: "#", current: false }];
+import { Link, useLocation } from "react-router-dom";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
 export default function Breadcrumbs() {
+  const location = useLocation();
+  const currentLink = "";
+
+  const crumbs = location.pathname
+    .split("/")
+    .filter((Boolean) => Boolean)
+    .map((crumb, index, array) => {
+      ` ${currentLink}/${crumb}`;
+      console.log(currentLink);
+      const isLastCrumb = index === array.length - 1;
+
+      return (
+        <div className="flex items-center gap-1" key={crumb}>
+          <Link className="text-neutral-500" to={currentLink}>
+            {crumb}
+          </Link>
+          {!isLastCrumb && (
+            <ChevronRightIcon
+              className="h-4 w-4 flex-shrink-0 text-neutral-500"
+              aria-hidden="true"
+            />
+          )}
+        </div>
+      );
+    });
+
   return (
-    <nav className="hidden lg:flex" aria-label="Breadcrumb">
-      <ol role="list" className="flex items-center space-x-1 z-10">
+    <nav className="hidden lg:flex gap-1 items-center" aria-label="Breadcrumb">
+      <Link
+        to="/"
+        className="text-sm text-neutral-500 hover:text-neutral-600 align-baseline"
+      >
+        Home
+      </Link>
+      <ChevronRightIcon
+        className="h-4 w-4 flex-shrink-0 text-neutral-500"
+        aria-hidden="true"
+      />
+      {crumbs}
+      {/* <ol className="flex items-center space-x-1 z-10">
         <li>
           <Link
             to="/"
@@ -33,7 +68,7 @@ export default function Breadcrumbs() {
             </div>
           </li>
         ))}
-      </ol>
+      </ol> */}
     </nav>
   );
 }
