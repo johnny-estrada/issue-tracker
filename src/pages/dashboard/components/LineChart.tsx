526d5ed6cbeed72046f29cab55038e002177810f
Johnny Estrada <johnnye562@yahoo.com>
2024-02-24 12:02:33 -0800

    Update LineChart responsiveness

--- src/pages/dashboard/components/LineChart.tsx
+++ src/pages/dashboard/components/LineChart.tsx
@@ -17,54 +17,116 @@ interface Props {
 
 function LineChart({ data }: Props) {
   return (
-    <div className="-ml-20">
-      <ResponsiveContainer width={500} height={160} aspect={2}>
-        <AreaChart
-          data={data}
-          margin={{ top: -30, right: 0, bottom: 25, left: -40 }}
-        >
-          <Legend
-            iconType="plainline"
-            iconSize={18}
-            wrapperStyle={{ marginInline: -50, marginBlock: -30, fontSize: 13 }}
-            align="center"
-            verticalAlign="bottom"
-          />
+    <>
+      <div className="lg:block hidden w-96 lg:w-full">
+        <ResponsiveContainer minWidth="100%" height={200}>
+          <AreaChart
+            data={data}
+            margin={{ top: -25, right: 50, bottom: 40, left: -30 }}
+          >
+            <Legend
+              iconType="plainline"
+              iconSize={18}
+              wrapperStyle={{
+                marginInline: -100,
+                marginBlock: 0,
+                fontSize: 13,
+              }}
+              align="center"
+              verticalAlign="bottom"
+            />
 
-          <defs>
-            <linearGradient
-              id="colorUv"
-              x1="0"
-              y1="0"
-              x2="0"
-              y2="1"
-            ></linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="2">
-              <stop offset="5%" stopColor="#FF7A50" stopOpacity={0.6} />
-              <stop offset="95%" stopColor="#FF7A50" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="date" />
-          <YAxis dataKey="completed" />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="created"
-            stroke="#333"
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
-          <Area
-            type="monotone"
-            dataKey="completed"
-            stroke="#FF7A50"
-            fillOpacity={1}
-            fill="url(#colorPv)"
-          />
-        </AreaChart>
-      </ResponsiveContainer>
-    </div>
+            <defs>
+              <linearGradient
+                id="colorUv"
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="1"
+              ></linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="2">
+                <stop offset="5%" stopColor="#FF7A50" stopOpacity={0.6} />
+                <stop offset="95%" stopColor="#FF7A50" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="date" />
+            <YAxis dataKey="completed" />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="created"
+              stroke="#333"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+            <Area
+              type="monotone"
+              dataKey="completed"
+              stroke="#FF7A50"
+              fillOpacity={1}
+              fill="url(#colorPv)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+      <div
+        className="block -ml-16 lg:hidden"
+        style={{ width: "100%", height: "100%" }}
+      >
+        <ResponsiveContainer width="100%" height={300}>
+          <AreaChart
+            data={data}
+
+            // margin={{ top: -30, right: 50, bottom: 50, left: 50 }}
+          >
+            <Legend
+              iconType="plainline"
+              iconSize={18}
+              wrapperStyle={{
+                marginInline: -50,
+                marginBlock: 0,
+                fontSize: 13,
+              }}
+              align="center"
+              verticalAlign="bottom"
+            />
+
+            <defs>
+              <linearGradient
+                id="colorUv"
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="1"
+              ></linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="2">
+                <stop offset="5%" stopColor="#FF7A50" stopOpacity={0.6} />
+                <stop offset="95%" stopColor="#FF7A50" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="date" />
+            <YAxis dataKey="completed" />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="created"
+              stroke="#333"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+            <Area
+              type="monotone"
+              dataKey="completed"
+              stroke="#FF7A50"
+              fillOpacity={1}
+              fill="url(#colorPv)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    </>
   );
 }
 
