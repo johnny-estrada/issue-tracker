7a758d4c09b5176a5fd384675338435d60dd839e
Johnny Estrada <johnnye562@yahoo.com>
2024-02-27 00:53:05 -0800

    Update ButtonGroup functionality

--- src/components/header/ButtonGroup.tsx
+++ src/components/header/ButtonGroup.tsx
@@ -1,25 +1,32 @@
+import { useState } from "react";
+
 // interface Props {
 //   titles: object[];
-//   title: string;
+//   onFilter: void;
 // }
 
 export default function ButtonGroup({ titles, onFilter }) {
+  const [active, setActive] = useState(titles[0]);
+
   return (
     <div className="isolate inline-flex rounded-md">
-      {titles?.map((title, i) => (
-        <>
-          <button
-            key={title.id}
-            type="button"
-            onClick={() => onFilter(title)}
-            value={title}
-            className="relative inline-flex items-center first:rounded-l-md last:rounded-r-md bg-white px-2.5 py-0 lg:px-3 
-            lg:py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 
-            focus:z-10 shadow-sm w-fit h-8"
-          >
-            {title}
-          </button>
-        </>
+      {titles?.map((title: string) => (
+        <button
+          key={title}
+          type="button"
+          onClick={() => {
+            setActive(title); // Set the active button
+            onFilter(title); // Trigger the filter function
+          }}
+          value={title}
+          className={
+            active === title
+              ? "relative inline-flex items-center first:rounded-l-md last:rounded-r-md bg-stone-800 px-2.5 py-0 lg:px-3 lg:py-2 text-sm text-white ring-1 ring-inset ring-stone-800 focus:z-10 shadow-sm w-fit h-8 pointer-events-none"
+              : "relative inline-flex items-center first:rounded-l-md last:rounded-r-md bg-white px-2.5 py-0 lg:px-3 lg:py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 shadow-sm w-fit h-8"
+          }
+        >
+          {title}
+        </button>
       ))}
     </div>
   );
