e311cbb30ddeff23366293d55fe212b8d2887758
Johnny Estrada <johnnye562@yahoo.com>
2024-03-03 11:40:19 -0800

    Update Settings import

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import EditTask from "./pages/tasks/components/EditTask";
 import TaskInformation from "./pages/tasks/TaskInformation";
 import Tasks from "./pages/tasks/Tasks";
 import Notifications from "./pages/notifications/Notifications";
-import Settings from "./pages/Settings";
+import Settings from "./pages/organization/Settings";
 import Login from "./pages/authentication/Login";
 import SignUp from "./pages/authentication/SignUp";
 
