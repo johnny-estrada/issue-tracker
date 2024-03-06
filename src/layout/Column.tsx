f5cb3a5709ae3687817b3b4bf83339d8bc57e684
Johnny Estrada <johnnye562@yahoo.com>
2024-03-05 22:13:56 -0800

    Update Column layout

--- src/layout/Column.tsx
+++ src/layout/Column.tsx
@@ -2,7 +2,7 @@ import Header from "../components/header/Header";
 import Sidebar from "../components/sidebar/Sidebar";
 
 const Layout = ({ children }) => {
-  const [headerTop, headerBottom, main] = children;
+  const [headerTop, headerMiddle, headerBottom, main] = children;
 
   return (
     <>
@@ -10,6 +10,7 @@ const Layout = ({ children }) => {
       <div className="grid grid-cols-1 overflow-hidden lg:flex flex-col lg:h-screen lg:ml-[288px]">
         <Header>
           {headerTop}
+          {headerMiddle}
           {headerBottom}
         </Header>
         <main className="lg:flex lg:h-screen">
