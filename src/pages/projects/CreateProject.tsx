512340e0d6056f7e731c21c7a290aa21d5f7c161
Johnny Estrada <johnnye562@yahoo.com>
2024-03-07 22:52:23 -0800

    Move CreateProject

--- src/pages/projects/CreateProject.tsx
+++ src/pages/projects/CreateProject.tsx
@@ -1,7 +1,7 @@
-import HeaderTitle from "../../../components/header/HeaderTitle";
-import CreateProjectForm from "./CreateProjectForm";
-import Header from "../../../components/header/Header";
-import Sidebar from "../../../components/sidebar/Sidebar";
+import HeaderTitle from "../../components/header/HeaderTitle";
+import CreateProjectForm from "./components/CreateProjectForm";
+import Header from "../../components/header/Header";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const EditProject = () => {
   return (
