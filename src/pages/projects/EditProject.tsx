02bcbd121664877872152e07e6d9f3d6eb788ad5
Johnny Estrada <johnnye562@yahoo.com>
2024-03-07 22:53:35 -0800

    Move EditProject

--- src/pages/projects/EditProject.tsx
+++ src/pages/projects/EditProject.tsx
@@ -1,7 +1,7 @@
-import HeaderTitle from "../../../components/header/HeaderTitle";
-import EditingTemplate from "./EditingTemplate";
-import Header from "../../../components/header/Header";
-import Sidebar from "../../../components/sidebar/Sidebar";
+import HeaderTitle from "../../components/header/HeaderTitle";
+import EditingTemplate from "./components/EditingTemplate";
+import Header from "../../components/header/Header";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const EditProject = () => {
   return (
