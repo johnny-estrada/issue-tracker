e46661ec5c8ccb9a65a174d67a9e131c6f574fec
Johnny Estrada <johnnye562@yahoo.com>
2024-03-12 20:57:41 -0700

    Add get route

--- backend/routes/attachmentRoutes.js
+++ backend/routes/attachmentRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 const router = express.Router();
-import upload from "../middleware/multerMiddleware.js"
-import { createAttachment } from "../controllers/attachmentController.js";
+import upload from "../middleware/multerMiddleware.js";
+import {
+  createAttachment,
+  getAttachments,
+} from "../controllers/attachmentController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(upload.single('file'), createAttachment);
+router.route("/").post(upload.single("file"), createAttachment);
+router.route("/").get(getAttachments);
 
 export default router;
