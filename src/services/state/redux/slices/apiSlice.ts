727a9714c376603a430f0079cc11d9a1a611f80d
Johnny Estrada <johnnye562@yahoo.com>
2024-03-12 21:00:33 -0700

    Add attachment to tagTypes

--- src/services/state/redux/slices/apiSlice.ts
+++ src/services/state/redux/slices/apiSlice.ts
@@ -4,6 +4,6 @@ const baseQuery = fetchBaseQuery({ baseUrl: "" });
 
 export const apiSlice = createApi({
   baseQuery,
-  tagTypes: ["User", "Project", "Tasks"],
+  tagTypes: ["User", "Project", "Task", "Attachment"],
   endpoints: (builder) => ({}),
 });
