d699142e02c31dc3e067868e6c2dca19c13c991f
Johnny Estrada <johnnye562@yahoo.com>
2024-03-12 20:56:22 -0700

    Format multerMiddleware

--- backend/middleware/multerMiddleware.js
+++ backend/middleware/multerMiddleware.js
@@ -1,15 +1,14 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-
-        cb(null, './public/Uploads/');
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "_" + file.originalname)
-    }
+  destination: (req, file, cb) => {
+    cb(null, "./public/Uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "_" + file.originalname);
+  },
 });
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 export default upload;
