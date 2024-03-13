7c9a6b454c43f102308c36223dd9742eab092fcf
Johnny Estrada <johnnye562@yahoo.com>
2024-03-12 20:55:31 -0700

    Add task and user association to attachments

--- backend/controllers/attachmentController.js
+++ backend/controllers/attachmentController.js
@@ -2,30 +2,69 @@
 import asyncHandler from "express-async-handler";
 import Attachment from "../models/attachmentModel.js";
 
+// @desc Fetch all attachments
+// @route GET /api/attachments
+// @access Private
+const getAttachments = asyncHandler(async (req, res) => {
+  const attachments = await Attachment.findAll();
+
+  if (attachments) {
+    res.status(200).json(attachments);
+  } else {
+    res.status(404);
+    throw new Error("Attachments not found");
+  }
+});
+
 // @desc Create attachment
 // @route POST /api/attachment
 // @access Private/Manager
 const createAttachment = asyncHandler(async (req, res) => {
-  const file = req.file;
+  const {
+    fieldname,
+    encoding,
+    originalname,
+    mimetype,
+    destination,
+    filename,
+    path,
+    size,
+  } = req.file;
 
-  console.log(file)
-  const newFile = Attachment.create({
-    fieldname: file.fieldname,
-    encoding: file.encoding,
-    originalname: file.originalname,
-    mimetype: file.mimetype,
-    destination: file.destination,
-    filename: file.filename,
-    path: file.path,
-    size: file.size
+  const { taskId, userId } = req.body;
+
+  // // Find the user's associated task
+  // const userTask = await Task.findOne({
+  //   where: { userId: userId, id: taskId },
+  // });
+  // console.log(userTask)
+
+  // // Check if a task is associated with the user
+  // if (!userTask) {
+  //   res.status(400);
+  //   throw new Error("User is not associated with a project");
+  // }
+
+  const newFile = await Attachment.create({
+    fieldname,
+    encoding,
+    originalname,
+    mimetype,
+    destination,
+    filename,
+    path,
+    size,
+    userId: userId,
+    taskId: taskId,
   });
 
   if (newFile) {
     res.status(201).json(newFile);
   } else {
+    console.error("Error creating attachment:", newFile);
     res.status(400);
     throw new Error("Invalid attachment data");
   }
 });
 
-export { createAttachment };
+export { getAttachments, createAttachment };
