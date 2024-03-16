d637162e116bc949629aacabcdd9244faf53a9bf
Johnny Estrada <johnnye562@yahoo.com>
2024-03-16 02:44:06 -0700

    Update SortBy selection

--- src/components/header/SortBy.tsx
+++ src/components/header/SortBy.tsx
@@ -13,9 +13,9 @@ const SortBy = () => {
         className="relative inline-flex items-center rounded-md bg-white px-3 text-sm font-normal text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 p-2"
       >
         <option value="status">status</option>
-        <option value="priority">priority</option>
+        {/* <option value="priority">priority</option>
         <option value="start date">start date</option>
-        <option value="target date">target date</option>
+        <option value="target date">target date</option> */}
       </select>
     </div>
   );
