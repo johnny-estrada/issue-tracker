e5a47277e12b5828529378ccf830744d9d0154bc
Johnny Estrada <johnnye562@yahoo.com>
2024-03-15 22:35:55 -0700

    Add status logic to FlatBadge

--- src/components/ui/FlatBadge.tsx
+++ src/components/ui/FlatBadge.tsx
@@ -1,15 +1,28 @@
 export default function FlatBadge(props) {
+  let badgeColor;
+
+  switch (props.priority) {
+    case "low":
+      badgeColor = "bg-green-50 text-green-800";
+      break;
+    case "medium":
+      badgeColor = "bg-yellow-50 text-yellow-800";
+      break;
+    case "high":
+      badgeColor = "bg-red-50 text-red-800";
+      break;
+    default:
+      badgeColor = "bg-green-50 text-green-800";
+      break;
+  }
+
   return (
     <>
-      <span className="inline-flex items-center rounded-md bg-green-50 px-3 text-xs font-medium text-green-800 h-8 w-fit">
+      <span
+        className={`inline-flex items-center rounded-md px-3 text-xs font-medium h-8 w-fit ${badgeColor}`}
+      >
         {props.priority}
       </span>
-      {/* <span className="inline-flex items-center rounded-md bg-red-50 px-3 text-xs font-medium text-red-800 h-8 w-fit">
-        high
-      </span>
-      <span className="inline-flex items-center rounded-md bg-orange-50 px-3 text-xs font-medium text-orange-800 h-8 w-fit">
-        medium
-      </span> */}
     </>
   );
 }
