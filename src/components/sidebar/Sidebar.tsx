81f73adf1cebc91e74cb6d985bb14f3278b39f05
Johnny Estrada <johnnye562@yahoo.com>
2024-03-15 22:38:19 -0700

    Minor update to margin top

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
   return (
     <>
       <aside className="fixed bottom-0 z-10 w-full lg:h-full lg:w-72 bg-neutral-800">
-        <div className="lg:mt-20 lg:pl-8">
+        <div className="lg:mt-12 lg:pl-8">
           <Logo />
         </div>
         <div className="lg:px-0 lg:mt-8">
