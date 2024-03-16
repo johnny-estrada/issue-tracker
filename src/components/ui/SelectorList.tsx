eb7d8408fbde367f461b573e1b31ff5900ba5c4b
Johnny Estrada <johnnye562@yahoo.com>
2024-03-15 22:40:37 -0700

    Minor update to SelectorList

--- src/components/ui/SelectorList.tsx
+++ src/components/ui/SelectorList.tsx
@@ -1,7 +1,7 @@
 interface Props {
   children: object[];
   id: string;
-  active: boolean;
+  active: boolean | undefined;
   onClick: (project: string) => void;
 }
 
@@ -11,17 +11,17 @@ export default function SelectorList({ children, id, active, onClick }: Props) {
       {children.length === 0 && <p>No items found</p>}
       <ul>
         <li>
-          <div
+          <button
             onClick={onClick}
             id={id}
             className={
               active
-                ? "flex justify-between gap-x-6 px-2 lg:px-4 py-2 lg:py-4 sm:px-6 bg-neutral-800  rounded-lg shadow ring-1 ring-gray-900/5 text-white w-full"
-                : "w-full flex cursor-pointer justify-between gap-x-6 px-2 lg:px-4 py-2 lg:py-4 hover:bg-gray-200 sm:px-6 bg-gray-100 rounded-lg shadow ring-1 ring-gray-200/5"
+                ? "flex justify-between gap-x-6 px-2 lg:px-2 py-2 lg:py-2 sm:px-6 bg-neutral-800  rounded-lg shadow ring-1 ring-gray-900/5 text-white w-full"
+                : "w-full flex cursor-pointer justify-between gap-x-6 px-2 lg:px-2 py-2 lg:py-2 hover:bg-gray-200 sm:px-6 bg-gray-100 rounded-lg shadow ring-1 ring-gray-200/5"
             }
           >
             {children}
-          </div>
+          </button>
         </li>
       </ul>
     </>
