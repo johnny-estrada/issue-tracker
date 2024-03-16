e7fdf577ac89ebff72a38705b1a3b9ce58d4f0a7
Johnny Estrada <johnnye562@yahoo.com>
2024-03-15 22:51:40 -0700

    Add additional fields to CreateTaskForm

--- src/pages/tasks/components/CreateTaskForm.tsx
+++ src/pages/tasks/components/CreateTaskForm.tsx
@@ -10,7 +10,10 @@ import { BarsArrowUpIcon, UsersIcon } from "@heroicons/react/20/solid";
 
 const CreateTaskForm = () => {
   const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
   const [status, setStatus] = useState("");
+  const [priority, setPriority] = useState("");
+  const [assignee, setAssignee] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [targetDate, setTargetDate] = useState(new Date());
   const [projectId, setProjectId] = useState("");
@@ -37,7 +40,9 @@ const CreateTaskForm = () => {
     try {
       await createTask({
         name,
+        description,
         status,
+        priority,
         startDate,
         targetDate,
         projectId,
@@ -54,6 +59,10 @@ const CreateTaskForm = () => {
     setStatus(e.target.value);
   };
 
+  const handlePriorityChange = (e) => {
+    setPriority(e.target.value);
+  };
+
   return (
     <>
       {loadingCreate && <Loader />}
@@ -65,26 +74,6 @@ const CreateTaskForm = () => {
           >
             <div className="px-4 py-6 sm:p-8">
               <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="title"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Name*
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="title"
-                      id="title"
-                      autoComplete="off"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                      className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
                 <div className="sm:col-span-4">
                   <label
                     htmlFor="project"
@@ -106,99 +95,124 @@ const CreateTaskForm = () => {
                           {project.title}-{project.id}
                         </option>
                       ))}
+                      <option>-</option>
                     </select>
                   </div>
                 </div>
-
-                {/* <div className="sm:col-span-3">
-              <label
-                htmlFor="description"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Description*
-              </label>
-              <div className="mt-2">
-                <textarea
-                  rows={4}
-                  name="description"
-                  id="description"
-                  autoComplete="description"
-                  defaultValue={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div> */}
-
-                {/* <div className="sm:col-span-4">
-              <label
-                htmlFor="client"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Client*
-              </label>
-              <div className="mt-2">
-                <input
-                  id="client"
-                  name="client"
-                  type="text"
-                  autoComplete="off"
-                  value={client}
-                  onChange={(e) => setClient(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div> */}
-
-                <div className="sm:col-span-4">
+                <div className="sm:col-span-3">
                   <label
-                    htmlFor="status"
+                    htmlFor="title"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Status
+                    Task name*
                   </label>
                   <div className="mt-2">
-                    <select
-                      id="status"
-                      name="status"
-                      autoComplete="status-name"
-                      value={status}
-                      onChange={handleStatusChange}
-                      className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    <input
+                      type="text"
+                      name="title"
+                      id="title"
+                      autoComplete="off"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="description"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Description*
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      rows={4}
+                      name="description"
+                      id="description"
+                      autoComplete="description"
+                      defaultValue={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-grow gap-10">
+                  <div>
+                    <label
+                      htmlFor="start-date"
+                      className="block text-sm font-medium leading-6 text-gray-900"
                     >
-                      <option value="To do">To do</option>
-                      <option value="In progress">In progress</option>
-                      <option value="Backlog">Backlog</option>
-                      <option value="Done">Done</option>
-                    </select>
+                      Start date
+                    </label>
+                    <div className="mt-2">
+                      <Datepicker
+                        startDate={startDate}
+                        setStartDate={setStartDate}
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="target-date"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Target date
+                    </label>
+                    <div className="mt-2">
+                      <Datepicker
+                        startDate={targetDate}
+                        setStartDate={setTargetDate}
+                      />
+                    </div>
                   </div>
                 </div>
-
-                <div className="col-span-full">
-                  <label
-                    htmlFor="start-date"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Start date
-                  </label>
-                  <div className="mt-2">
-                    <Datepicker
-                      startDate={startDate}
-                      setStartDate={setStartDate}
-                    />
+                <div className="flex flex-grow gap-10">
+                  <div className="sm:col-span-4 flex-1">
+                    <label
+                      htmlFor="priority"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Priority
+                    </label>
+                    <div className="mt-2">
+                      <select
+                        id="priority"
+                        name="priority"
+                        autoComplete="status-name"
+                        value={priority}
+                        onChange={handlePriorityChange}
+                        className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      >
+                        <option value="Low">low</option>
+                        <option value="Medium">medium</option>
+                        <option value="High">high</option>
+                      </select>
+                    </div>
                   </div>
-
-                  <label
-                    htmlFor="target-date"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Target date
-                  </label>
-                  <div className="mt-2">
-                    <Datepicker
-                      startDate={targetDate}
-                      setStartDate={setTargetDate}
-                    />
+                  <div className="sm:col-span-4 flex-1">
+                    <label
+                      htmlFor="status"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Status
+                    </label>
+                    <div className="mt-2">
+                      <select
+                        id="status"
+                        name="status"
+                        autoComplete="status-name"
+                        value={status}
+                        onChange={handleStatusChange}
+                        className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      >
+                        <option value="To do">To do</option>
+                        <option value="In progress">In progress</option>
+                        <option value="Backlog">Backlog</option>
+                        <option value="Done">Done</option>
+                      </select>
+                    </div>
                   </div>
                 </div>
 
@@ -207,7 +221,7 @@ const CreateTaskForm = () => {
                     htmlFor="postal-code"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Members
+                    Asignee
                   </label>
                   <PeoplePicker
                     members={members}
@@ -216,61 +230,6 @@ const CreateTaskForm = () => {
                     }
                   />
                 </div>
-
-                {/* <div className="sm:col-span-2">
-              <label
-                htmlFor="region"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Hours
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="hours"
-                  id="hours"
-                  value={hours}
-                  onChange={(e) => setHours(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div> */}
-
-                {/* <div className="sm:col-span-2">
-              <label
-                htmlFor="postal-code"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Members
-              </label>
-              <div className="mt-2 flex rounded-md shadow-sm">
-                <div className="relative flex flex-grow items-stretch focus-within:z-10">
-                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <UsersIcon
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="block w-full rounded-none rounded-l-md border-0 py-1.5 px-3 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="John Smith"
-                  />
-                </div>
-                <button
-                  type="button"
-                  className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  <BarsArrowUpIcon
-                    className="-ml-0.5 h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                  Sort
-                </button>
-              </div>
-            </div> */}
               </div>
             </div>
             <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
