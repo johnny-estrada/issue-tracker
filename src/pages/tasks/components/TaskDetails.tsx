55b15e04094642a575e2d12f1ee21e25d5bd3239
Johnny Estrada <johnnye562@yahoo.com>
2024-03-21 23:02:00 -0700

    Refactor TaskDetails

--- src/pages/tasks/components/TaskDetails.tsx
+++ src/pages/tasks/components/TaskDetails.tsx
@@ -1,6 +1,21 @@
 import { NavLink } from "react-router-dom";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/20/solid";
 import FlatBadge from "../../../components/ui/FlatBadge";
+import { formatDate } from "../../../utils/formatting";
+import Attachments from "./Attachments";
+import ActivityLog from "./ActivityLog";
+import noData from "../../../assets/images/no-data.svg";
+
+interface Props {
+  tasks: object[];
+  taskIndex: number;
+  projects: object[];
+  formattedDate1: string;
+  formattedDate2: string;
+  users: object[];
+  taskId: number;
+  userId: number;
+}
 
 const TaskDetails = ({
   tasks,
@@ -8,16 +23,44 @@ const TaskDetails = ({
   projects,
   formattedDate1,
   formattedDate2,
-}) => {
+  users,
+  taskId,
+  userId,
+}: Props) => {
+  const userIndex = users?.findIndex(
+    (user) => user.id === tasks[taskIndex].userId,
+  );
+
+  // console.log(task1);
+  // console.log(tasks[taskIndex].userId); // 4
+
+  const created = formatDate(tasks[taskIndex].createdAt, {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    time: "numeric",
+  });
+
+  const updated = formatDate(tasks[taskIndex].updatedAt, {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  if (!users) {
+    // Handle the case where tasks is undefined or projectIndex is out of bounds
+    return null; // or you can render an error message
+  }
+
   return (
     <>
-      <section aria-labelledby="tasks">
+      <section aria-labelledby="tasks" className="px-4">
         <h2 className="sr-only" id="tasks">
           All Tasks
         </h2>
         <div className="flex justify-between">
-          <div className="flex gap-5">
-            <h2 className="text-xl pb-4">Task details</h2>
+          <div className="flex gap-5 items-baseline">
+            <h2 className="text-xl lg:text-2xl pb-4">Task details</h2>
             <FlatBadge priority={`ID LG-${tasks?.[taskIndex]?.id}`} />
           </div>
 
@@ -36,44 +79,48 @@ const TaskDetails = ({
             />
           </NavLink>
         </div>
-        <div className="w-2/3">
+        <div className=" lg:mb-10">
           {tasks?.length > 0 ? (
-            <ul>
-              <li className="grid grid-cols-2 py-2 w-64">
-                <h4 className="text-sm text-neutral-500">Name</h4>
-                <p className="text-sm">{tasks[taskIndex]?.name}</p>
+            <ul className="w-2/3">
+              <li className="flex justify-center py-2 items-center w-full">
+                <h4 className="text-sm text-neutral-500 w-36">Name</h4>
+                <p className="flex-1 text-sm w-64 line-clamp-1">
+                  {tasks[taskIndex]?.name}
+                </p>
               </li>
-              <li className="grid grid-cols-2 py-2 w-64">
-                <h4 className="text-sm text-neutral-500">Status</h4>
-                <p className="text-sm">{tasks[taskIndex]?.status}</p>
+              <li className="flex justify-center items-center py-2 w-72">
+                <h4 className="text-sm text-neutral-500 w-36">Status</h4>
+                <p className="flex-1 text-sm w-64 line-clamp-1">
+                  {tasks[taskIndex]?.status}
+                </p>
               </li>
-              <li className="grid grid-cols-2 py-2 w-64">
-                <h4 className="text-sm text-neutral-500">Priority</h4>
+              <li className="flex items-center py-2 w-64">
+                <h4 className="text-sm text-neutral-500 w-36">Priority</h4>
 
                 <FlatBadge priority={tasks[taskIndex]?.priority} />
               </li>
-              <li className="grid grid-cols-2 py-2 w-64">
-                <h4 className="text-sm text-neutral-500">Assignee</h4>
-                <div className="flex gap-3">
+              <li className="flex items-center py-2 w-64">
+                <h4 className="text-sm text-neutral-500 w-36">Assignee</h4>
+                <div className="flex gap-3 items-center">
                   <img
                     className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-                    src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    src={projects[0].team[0].photo}
                     alt=""
                   />
-                  <p className="text-sm">Fred S</p>
+                  <p className="text-sm">{projects[0].team[0].name}</p>
                 </div>
               </li>
-              <li className="grid grid-cols-2 py-2 w-64">
-                <h4 className="text-sm text-neutral-500">Start date</h4>
+              <li className="flex items-center py-2 w-64">
+                <h4 className="text-sm text-neutral-500 w-36">Start date</h4>
                 <p className="text-sm">{formattedDate1}</p>
               </li>
-              <li className="grid grid-cols-2 py-2 w-64">
-                <h4 className="text-sm text-neutral-500">Target date</h4>
+              <li className="flex items-center py-2 w-64">
+                <h4 className="text-sm text-neutral-500 w-36">Target date</h4>
                 <p className="text-sm">{formattedDate2}</p>
               </li>
-              <li className="grid grid-cols-2 py-2 w-64">
-                <h4 className="text-sm text-neutral-500">Project</h4>
-                <p className="text-sm">
+              <li className="flex items-center py-2 w-64">
+                <h4 className="text-sm text-neutral-500 w-36">Project</h4>
+                <p className="flex-1 text-sm w-60 line-clamp-1">
                   {
                     projects?.find(
                       (project) => tasks[taskIndex]?.projectId === project.id,
@@ -83,10 +130,25 @@ const TaskDetails = ({
               </li>
             </ul>
           ) : (
-            <li>No task details available</li>
+            <div className="flex flex-col justify-center items-center mt-10">
+              <img src={noData} alt="" className="w-36 mb-3" />
+              <p>No task details available</p>
+            </div>
           )}
         </div>
       </section>
+      <Attachments
+        taskId={taskId}
+        userId={userId}
+        taskIndex={taskIndex}
+        tasks={tasks}
+      />
+      <ActivityLog
+        created={created}
+        users={users}
+        userIndex={userIndex}
+        updated={updated}
+      />
     </>
   );
 };
