17a0b5393b46539c6a11fed7e2d440c3f156a5db
Johnny Estrada <johnnye562@yahoo.com>
2024-03-21 23:43:06 -0700

    Update MyTask

--- src/pages/dashboard/components/MyTask.tsx
+++ src/pages/dashboard/components/MyTask.tsx
@@ -2,13 +2,23 @@ import { Link } from "react-router-dom";
 import ShowHideList from "../components/ShowHideList";
 import AvatarGroup from "../../../components/ui/AvatarGroup";
 import Menu1 from "../../../components/ui/Menu1";
+import Tabs from "../../../components/ui/Tabs";
 import {
   PencilSquareIcon,
   PlusIcon,
   TrashIcon,
 } from "@heroicons/react/24/outline";
 
-const MyTask = ({ id, projectIndex, projects, tasks, dates, refetch }) => {
+const MyTask = ({
+  id,
+  projectIndex,
+  projects,
+  tasks,
+  dates,
+  refetch,
+  lineChartData,
+  formattedDates,
+}) => {
   const NAV = [
     {
       name: "Create task",
@@ -24,7 +34,8 @@ const MyTask = ({ id, projectIndex, projects, tasks, dates, refetch }) => {
 
   return (
     <>
-      <section aria-labelledby="projects">
+      <Tabs data={lineChartData} projectData={projects} />
+      <section aria-labelledby="projects" className="hidden lg:block">
         <header className="hidden lg:flex justify-between pb-5">
           <div className="flex items-baseline gap-3">
             <h2 className="sr-only" id="tasks">
@@ -48,6 +59,7 @@ const MyTask = ({ id, projectIndex, projects, tasks, dates, refetch }) => {
         </header>
 
         <ShowHideList
+          formattedDates={formattedDates}
           projectIndex={projectIndex}
           projects={projects}
           tasks={tasks}
