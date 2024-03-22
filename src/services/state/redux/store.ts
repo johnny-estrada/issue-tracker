211544df6927985466d588137b49229836e490f3
Johnny Estrada <johnnye562@yahoo.com>
2024-03-21 23:00:24 -0700

    Add search reducer

--- src/services/state/redux/store.ts
+++ src/services/state/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import searchReducer from "./slices/searchSlice";
 import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    search: searchReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
