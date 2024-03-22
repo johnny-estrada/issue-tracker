461ef38c6d4213e14923c58a90efce8717f743c4
Johnny Estrada <johnnye562@yahoo.com>
2024-03-21 23:03:24 -0700

    Update Breadcrumbs currentLink

--- src/components/header/Breadcrumbs.tsx
+++ src/components/header/Breadcrumbs.tsx
@@ -3,13 +3,12 @@ import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
 export default function Breadcrumbs() {
   const location = useLocation();
-  const currentLink = "";
 
   const crumbs = location.pathname
     .split("/")
-    .filter((Boolean) => Boolean)
+    .filter(Boolean)
     .map((crumb, index, array) => {
-      ` ${currentLink}/${crumb}`;
+      const currentLink = `/${array.slice(0, index + 1).join("/")}`;
       const isLastCrumb = index === array.length - 1;
 
       return (
@@ -40,34 +39,6 @@ export default function Breadcrumbs() {
         aria-hidden="true"
       />
       {crumbs}
-      {/* <ol className="flex items-center space-x-1 z-10">
-        <li>
-          <Link
-            to="/"
-            className="text-sm text-neutral-500 hover:text-neutral-600 align-baseline"
-          >
-            Home
-          </Link>
-        </li>
-        {pages.map((page) => (
-          <li key={page.name}>
-            <div className="flex items-center">
-              <ChevronRightIcon
-                className="h-4 w-4 flex-shrink-0 text-neutral-500"
-                aria-hidden="true"
-              />
-              <Link
-                to={page.to}
-                className="text-sm ml-1 text-neutral-500
-                hover:text-gray-700 align-baseline"
-                aria-current={page.current ? "page" : undefined}
-              >
-                {page.name}
-              </Link>
-            </div>
-          </li>
-        ))}
-      </ol> */}
     </nav>
   );
 }
