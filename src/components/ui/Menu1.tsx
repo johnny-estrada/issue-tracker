c4683a06f529ed06886ecc0c7f3e403c673a4b2e
Johnny Estrada <johnnye562@yahoo.com>
2024-03-21 23:09:58 -0700

    Minor update to Menu.Items component style

--- src/components/ui/Menu1.tsx
+++ src/components/ui/Menu1.tsx
@@ -38,7 +38,7 @@ export default function Menu1({ navigation, id, delete1 }) {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute -translate-x-44 left mt-2 w-56 p-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+          <Menu.Items className="absolute -translate-x-44 left mt-2 w-56 p-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none z-20">
             <div className="px-1 py-1">
               {navigation.map((item, index) => (
                 <Menu.Item key={index}>
