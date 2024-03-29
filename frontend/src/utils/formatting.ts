e466fccba8f33b5c9c3d4d5ae7d2132a5b7671e1
Johnny Estrada <johnnye562@yahoo.com>
2024-03-29 16:40:59 -0700

    Add interfaces to formatting functions

--- frontend/src/utils/formatting.ts
+++ frontend/src/utils/formatting.ts
@@ -1,17 +1,25 @@
 // Function that is small, helpful with formatting.
 //Ex. Currencies, dates, numbers, etc. or 3ed party libraries.
+interface Props {
+  dateString: string;
+  options: string;
+}
 
-export const formatDate = (dateString, options) => {
+export const formatDate = ({ dateString, options }: Props) => {
   const date = new Date(dateString);
   return date.toLocaleDateString("en-US", options);
 };
 
-export const formatName = (fullName) => {
+interface Props {
+  fullName: string;
+}
+
+export const formatName = ({fullName}: Props) => {
   const firstName = fullName.split(" ");
   return firstName[0];
 };
 
-export const formatNameDisplay = (fullName) => {
+export const formatNameDisplay = ({fullName}: Props) => {
   const parts = fullName.split(" ");
   const firstName = parts[0];
   const lastNameInitial =
