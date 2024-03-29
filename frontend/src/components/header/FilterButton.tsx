5898a61094de1dbd62d804af336427653fa33b3c
Johnny Estrada <johnnye562@yahoo.com>
2024-03-28 17:25:50 -0700

    Update funnel icon

--- frontend/src/components/header/FilterButton.tsx
+++ frontend/src/components/header/FilterButton.tsx
@@ -1,5 +1,5 @@
-import { FunnelIcon } from "@heroicons/react/24/outline";
 import { AdjustmentsVerticalIcon } from "@heroicons/react/24/solid";
+import funnelIcon from "../../assets/images/icons/funnel.svg"
 
 export default function FilterButton() {
   return (
@@ -13,7 +13,8 @@ export default function FilterButton() {
         className="hidden lg:flex justify-center align-middle mt-auto rounded-md bg-white px-3 py-1.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
       >
         <div className="flex justify-center align-middle gap-1">
-          <FunnelIcon className="h-4 w-4" />
+          {/* <FunnelIcon className="h-4 w-4" /> */}
+          <img src={funnelIcon} alt="" className="h-5 w-5" />
           <span className="text-sm">Filter</span>
         </div>
       </button>
