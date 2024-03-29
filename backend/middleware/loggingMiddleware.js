875a55af217c4e9b35a95f19327b8e24694f0a1e
Johnny Estrada <johnnye562@yahoo.com>
2024-03-29 13:56:43 -0700

    Create loggingMiddleware

---
+++ backend/middleware/loggingMiddleware.js
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+import morgan from "morgan";
+
+export const setupLogging = (app) => {
+  const logFilePath = path.join(process.cwd(), "logs", "morgan.log");
+  const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
+  const logFormat =
+    ":date[web] :method :url :status :response-time ms - :res[content-length]\n";
+
+  morgan.token("date", (req, re) => {
+    const pacificOptions = {
+      timeZone: "America/Los_Angeles",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: true,
+    };
+    return new Date().toLocaleString("en-US", pacificOptions);
+  });
+
+  // Add log rotation functionality
+  // have to install a package
+
+  app.use(
+    morgan(logFormat, {
+      stream: logStream,
+      skip: (req, res) => res.statusCode < 400,
+    }),
+  );
+};
