0c572ed234f762613bd8f2ebe994d6ae9d97af50
Johnny Estrada <johnnye562@yahoo.com>
2024-04-02 14:13:51 -0700

    Add module declaration for cloudinary-react

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module "cloudinary-react" {
+  interface ImageProps extends React.HTMLAttributes<HTMLImageElement> {
+    publicId: string;
+    cloudName: string;
+  }
+
+  export class Image extends React.Component<ImageProps, object> {}
+}
