f99062840f2f0b15890a4735bc895e56ee58283c
Johnny Estrada <johnnye562@yahoo.com>
2024-04-03 23:07:58 -0700

    Remove mobile nav

--- frontend/src/components/header/SearchBar.tsx
+++ frontend/src/components/header/SearchBar.tsx
@@ -26,51 +26,7 @@ const SearchBar = () => {
 
   return (
     <>
-      {/* Mobile Top Nav */}
-      {/* <div className="bg-gray-100 m-auto w-1/2"> */}
-      <div className="lg:hidden flex items-center gap-4 text-gray-900 justify-end z-10 mb-2">
-        <input
-          type="text"
-          name="mobile-search"
-          id="mobile-search"
-          className="hidden absolute top-5 left-0 w-full rounded-full py-1.5 pl-10 bg-white text-sm leading-6 text-gray-900 ring-1 ring-inset ring-slate-300 placeholder:text-slate-500 focus:ring-2 focus:ring-inset focus:ring-sky-600 outline-none lg:block shadow-sm h-12"
-          placeholder="Search"
-          value={search}
-          onChange={handleChange}
-          onSubmit={handleSubmit}
-        />
-        {searchResults.length === 0 ? (
-          <p>No results found</p>
-        ) : search && !isLoading ? (
-          <ul className="absolute top-[70px] left-0 ring-1 ring-gray-100 rounded shadow-sm w-full bg-white">
-            {searchResults
-              .filter(
-                (task: Task) =>
-                  task.name.toLowerCase().includes(search.toLowerCase()) ||
-                  task.name.toUpperCase().includes(search.toUpperCase()) ||
-                  task.id.toString().includes(search),
-              )
-              .map((task: Task) => (
-                <li key={task.id} className="hover:bg-gray-50 p-4">
-                  <Link className="text-sm" to={`/tasks/${task.id}`}>
-                    <p className="line-clamp-1">
-                      <span className="font-semibold">ID TK-{task.id}</span>{" "}
-                      {task.name}{" "}
-                    </p>
-                  </Link>
-                </li>
-              ))}
-          </ul>
-        ) : null}
-        <button className="">
-          <MagnifyingGlassIcon className="w-5 h-5 lg:hidden" />
-        </button>
-        <Link to="/notifications">
-          <BellIcon className="w-5 h-5 lg:hidden" />
-        </Link>
-      </div>
-
-      <div className="hidden lg:flex mt-auto rounded-md z-10">
+      <div className="flex mt-auto rounded-md z-10">
         <form>
           <div className="relative z-10">
             <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
@@ -127,7 +83,7 @@ const SearchBar = () => {
         {/* Mobile create button */}
         <Link
           to={`/projects/create-project`}
-          className="lg:hidden fixed bottom-20 right-3 bg-orange-500 w-10 h-10 rounded-full drop-shadow-lg text-white text-3xl hover:drop-shadow-2xl hover:bg-orange-400 z-10 flex justify-center items-center"
+          className="lg:hidden fixed bottom-20 right-3 bg-orange-500 w-10 h-10 rounded-full drop-shadow-lg text-white text-2xl hover:drop-shadow-2xl hover:bg-orange-400 z-10 flex justify-center items-center"
         >
           +
         </Link>
