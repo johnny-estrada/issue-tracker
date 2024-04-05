89941475e8da7b040828a0e07ea0d6f3fe5a1f11
Johnny Estrada <johnnye562@yahoo.com>
2024-04-04 20:59:37 -0700

    Add no file provided check

--- frontend/src/hooks/useCloudinaryUpload.ts
+++ frontend/src/hooks/useCloudinaryUpload.ts
@@ -9,25 +9,27 @@ interface Props {
 const useCloudinaryUpload = ({ presetKey, cloudName }: Props) => {
   const [photo, setPhoto] = useState("");
 
-  const uploadFile = async (file: string) => {
-    try {
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("upload_preset", presetKey);
+  const uploadFile = async (file: File | null) => {
+    if (file) {
+      try {
+        const formData = new FormData();
+        formData.append("file", file!); // Use the non-null assertion operator
+        formData.append("upload_preset", presetKey);
 
-      const response = await Axios.post(
-        `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-        formData,
-      );
+        const response = await Axios.post(
+          `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+          formData,
+        );
 
-      const imageName = response.data.url;
-      setPhoto(imageName);
-      // You can return or handle the photo URL as needed
-      return imageName;
-    } catch (error) {
-      console.error("Error uploading image:", error);
-      // Handle the error, e.g., show a toast or alert
-      throw error;
+        const imageName = response.data.url;
+        setPhoto(imageName);
+        return imageName;
+      } catch (error) {
+        console.error("Error uploading image:", error);
+        throw error;
+      }
+    } else {
+      throw new Error("No file provided for upload");
     }
   };
 
