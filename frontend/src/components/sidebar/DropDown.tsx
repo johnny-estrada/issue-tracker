bfb6bd54865386e247a9836efb18a6b1247090dd
Johnny Estrada <johnnye562@yahoo.com>
2024-04-09 18:21:57 -0700

    Update DropDown transition location

--- frontend/src/components/sidebar/DropDown.tsx
+++ frontend/src/components/sidebar/DropDown.tsx
@@ -84,7 +84,7 @@ export default function DropDown() {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute -right-32 -top-28 mt-2 w-56 p-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+          <Menu.Items className="absolute -right-32 -top-14 mt-2 w-56 p-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
             <div className="px-1 py-1">
               <Menu.Item>
                 {({ active }) => (
