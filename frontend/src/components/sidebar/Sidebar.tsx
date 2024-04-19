b17c4f438933c2a0fd4bc8154cf8877016888c40
Johnny Estrada <johnnye562@yahoo.com>
2024-04-18 22:39:42 -0700

    Update sidebar z index

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { navigation, mobileNavigation } from "../../data/index";
 export default function Sidebar() {
   return (
     <>
-      <aside className="fixed bottom-0 z-10 w-full lg:h-full lg:w-72 bg-neutral-800">
+      <aside className="fixed bottom-0 z-20 w-full lg:h-full lg:w-72 bg-neutral-800">
         <div className="lg:mt-14 lg:mb-12 lg:pl-8">
           <Logo />
         </div>
