e170cb4c7c8567e3c621a8a9a57ef0442fc2341f
Johnny Estrada <johnnye562@yahoo.com>
2024-05-06 22:58:26 -0700

    Update Security font size

--- frontend/src/pages/Settings/Security.tsx
+++ frontend/src/pages/Settings/Security.tsx
@@ -24,13 +24,13 @@ const Security = ({
 
         <h2 className="text-xl lg:text-2xl pb-4">Security</h2>
       </header>
-      <form method="PUT" onSubmit={submitHandler} className="bg-white mb-10">
+      <form method="PUT" onSubmit={submitHandler} className="bg-white mb-5">
         <div className="pb-4">
-          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-6">
             <div>
               <label
                 htmlFor="password"
-                className="block text-sm font-medium leading-6 text-slate-500"
+                className="block text-sm leading-6 text-slate-500"
               >
                 New password
               </label>
@@ -51,7 +51,7 @@ const Security = ({
             <div>
               <label
                 htmlFor="confirmPassword"
-                className="block text-sm font-sm leading-6 text-slate-500"
+                className="block text-sm leading-6 text-slate-500"
               >
                 Confirm password
               </label>
