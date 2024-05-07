c5032acb71d7fdd7971cd46b5419351af585975e
Johnny Estrada <johnnye562@yahoo.com>
2024-05-06 22:53:31 -0700

    Update Sidebar layout

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -6,17 +6,28 @@ import { navigation, mobileNavigation } from "../../data/index";
 export default function Sidebar() {
   return (
     <>
-      <aside className="fixed bottom-0 z-20 w-full lg:h-full lg:w-72 bg-neutral-800">
-        <div className="lg:mt-14 lg:mb-12 lg:pl-8">
+      <aside className="w-full lg:h-full lg:w-72 bg-neutral-800">
+        <div className="lg:pt-14 lg:mb-12 lg:pl-8">
           <Logo />
         </div>
-        <div className="flex items-center justify-center lg:block">
+        <div className="flex items-center justify-center lg:block h-full">
           <Navigation
             navigation={navigation}
             mobileNavigation={mobileNavigation}
           />
         </div>
       </aside>
+      {/* <aside className="fixed bottom-0 z-20 w-full lg:h-full lg:w-72 bg-neutral-800">
+        <div className="lg:mt-14 lg:mb-12 lg:pl-8">
+          <Logo />
+        </div>
+        <div className="flex items-center justify-center lg:block h-full">
+          <Navigation
+            navigation={navigation}
+            mobileNavigation={mobileNavigation}
+          />
+        </div>
+      </aside> */}
     </>
   );
 }
