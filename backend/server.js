7bab45e7ee09c49d047fbfdc044c9613a4155735
Johnny Estrada <johnnye562@yahoo.com>
2024-05-24 14:15:10 -0700

    Minor update to server

--- backend/server.js
+++ backend/server.js
@@ -25,12 +25,13 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 setupLogging(app);
-setStaticFile(app);
 
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/attachments", attachmentRoutes);
+
+setStaticFile(app);
 
 app.use(notFound);
 app.use(errorHandler);
