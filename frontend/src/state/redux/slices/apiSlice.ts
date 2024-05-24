3fecbda3bc913490737f62fdd8ac880832713035
Johnny Estrada <johnnye562@yahoo.com>
2024-05-24 14:48:06 -0700

    Uncomment timeout logout check

--- frontend/src/state/redux/slices/apiSlice.ts
+++ frontend/src/state/redux/slices/apiSlice.ts
@@ -1,22 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { logout } from "./authSlice";
+import { logout } from "./authSlice";
 
 const baseQuery = fetchBaseQuery({ baseUrl: "" });
 
-// async function baseQueryWithAuth(args, api, extra) {
-//   try {
-//     const result = await baseQuery(args, api, extra);
+async function baseQueryWithAuth(args, api, extra) {
+  try {
+    const result = await baseQuery(args, api, extra);
 
-//     if (result.error && result.error.status === 401) {
-//       api.dispatch(logout());
-//     } return result;
-//   } catch (error) {
-//     return { error }
-//   }
-// }
+    if (result.error && result.error.status === 401) {
+      api.dispatch(logout());
+    } return result;
+  } catch (error) {
+    return { error }
+  }
+}
 
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery: baseQueryWithAuth,
   tagTypes: ["User", "Project", "Task", "Attachment"],
   endpoints: (_builder) => ({}),
 });
