d4268d78188f92400ea562ca0b8c3b595e8b416f
Johnny Estrada <johnnye562@yahoo.com>
2024-06-05 14:05:26 -0700

    Add setup model to server

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import userRoutes from "./routes/userRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import attachmentRoutes from "./routes/attachmentRoutes.js";
-
+import { setupModels } from "./models/index.js";
 import { setupLogging } from "./middleware/loggingMiddleware.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import { setStaticFile } from "./middleware/staticFileMiddleware.js";
@@ -24,13 +24,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
 
-setupLogging(app);
-
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/attachments", attachmentRoutes);
 
+setupModels();
+setupLogging(app);
 setStaticFile(app);
 
 app.use(notFound);
