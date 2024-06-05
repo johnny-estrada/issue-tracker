9a0c0c553ef9c6658d5faac6f54faddc97652fd1
Johnny Estrada <johnnye562@yahoo.com>
2024-06-05 14:07:07 -0700

    Remove model setup logic

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,5 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
-import bcrypt from "bcrypt";
-import Project from "./projectModel.js";
 
 const User = sequelize.define(
   "User",
@@ -41,40 +39,5 @@ const User = sequelize.define(
     timestamps: true,
   }
 );
-
-User.beforeCreate(async (user) => {
-  try {
-    const hashedPassword = await bcrypt.hash(user.password, 12);
-    user.password = hashedPassword;
-  } catch (error) {
-    throw new Error("Error hashing the password.", error);
-  }
-});
-
-// This hook checks if the password field has changed before hashing it again
-User.beforeUpdate(async (user) => {
-  if (user.changed("password")) {
-    const hashedPassword = await bcrypt.hash(user.password, 12);
-    user.password = hashedPassword;
-  }
-});
-
-User.prototype.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-const setupAssociations = () => {
-  User.hasMany(Project, { foreignKey: "userId" });
-};
-
-export { setupAssociations };
-
-User.sync()
-  .then(() => {
-    console.log("User table created successfully!");
-  })
-  .catch((error) => {
-    console.error("Unable to create table : ", error);
-  });
 
 export default User;
