c9dc24c8e4a6965625e5acb0dd15bf077fa8125f
Johnny Estrada <johnnye562@yahoo.com>
2024-06-06 13:51:01 -0700

    Move loader to top of file

--- frontend/src/pages/authentication/SignUp.tsx
+++ frontend/src/pages/authentication/SignUp.tsx
@@ -54,6 +54,7 @@ export default function SignUp() {
 
   return (
     <>
+      {isLoading && <Loader />}
       <div className="flex flex-1 min-h-screen">
         <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
@@ -182,7 +183,6 @@ export default function SignUp() {
                   </div>
 
                   <div>
-                    {isLoading && <Loader />}
                     <button
                       type="submit"
                       className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
