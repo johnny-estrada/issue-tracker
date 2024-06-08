8f48c755641f4223f331ff6150af6a9ee378de80
Johnny Estrada <johnnye562@yahoo.com>
2024-06-08 12:45:07 -0700

    Update Breadcrumbs logic and color

--- frontend/src/components/ui/header/Breadcrumbs.tsx
+++ frontend/src/components/ui/header/Breadcrumbs.tsx
@@ -10,17 +10,18 @@ export default function Breadcrumbs() {
     .map((crumb, index, array) => {
       const currentLink = `/${array.slice(0, index + 1).join("/")}`;
       const isLastCrumb = index === array.length - 1;
+      const capitalizedCrumb = crumb.charAt(0).toUpperCase() + crumb.slice(1);
 
       return (
         <div className="flex items-center gap-1" key={crumb}>
-          <Link className="text-neutral-500" to={currentLink}>
-            {crumb}
+          <Link
+            className="text-slate-600 hover:text-slate-500 text-sm lg:text-base"
+            to={currentLink}
+          >
+            {capitalizedCrumb}
           </Link>
           {!isLastCrumb && (
-            <ChevronRightIcon
-              className="h-4 w-4 flex-shrink-0 text-neutral-500"
-              aria-hidden="true"
-            />
+            <ChevronRightIcon className="h-4 w-4 text-slate-600" aria-hidden="true" />
           )}
         </div>
       );
@@ -30,12 +31,12 @@ export default function Breadcrumbs() {
     <nav className="hidden lg:flex gap-1 items-center" aria-label="Breadcrumb">
       <Link
         to="/"
-        className="text-sm text-neutral-500 hover:text-neutral-600 align-baseline"
+        className="text-sm lg:text-base text-slate-600 hover:text-slate-500 align-baseline"
       >
         Home
       </Link>
       <ChevronRightIcon
-        className="h-4 w-4 flex-shrink-0 text-neutral-500"
+        className="h-4 w-4 flex-shrink-0 text-slate-600"
         aria-hidden="true"
       />
       {breadcrumbs}
