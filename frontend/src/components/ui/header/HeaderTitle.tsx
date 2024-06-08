980a548cbf1bf066ed2c4a2dfaf09797735b7682
Johnny Estrada <johnnye562@yahoo.com>
2024-06-08 12:45:43 -0700

    Update HeaderTitle display logic

--- frontend/src/components/ui/header/HeaderTitle.tsx
+++ frontend/src/components/ui/header/HeaderTitle.tsx
@@ -1,19 +1,32 @@
 import Breadcrumbs from "./Breadcrumbs";
 import Greeting from "../../../pages/Dashboard/components/Greeting";
 
-interface Props {
+interface IHeaderTitle {
   title: string;
+  active: boolean;
 }
 
-const HeaderTitle = ({ title }: Props) => {
+const HeaderTitle = ({ title, active }: IHeaderTitle) => {
   return (
     <>
-      <div className="w-full">
-        {title === "Dashboard" ? <Greeting /> : <Breadcrumbs />}
-        <h1 className="text-3xl lg:text-4xl text-gray-900 line-clamp-1">
-          {title}
-        </h1>
-      </div>
+      {title && active ? (
+        <>
+          <div className="w-full">
+            {title === "Dashboard" ? <Greeting /> : <Breadcrumbs />}
+            <h1 className="text-3xl lg:text-4xl text-gray-900 line-clamp-1">
+              {title}
+            </h1>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="w-full">
+            <h1 className="text-3xl lg:text-4xl text-gray-900 line-clamp-1">
+              {title}
+            </h1>
+          </div>
+        </>
+      )}
     </>
   );
 };
