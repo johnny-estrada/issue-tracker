3a448f49702d87dbeffbe30949becce43b41b2e9
Johnny Estrada <johnnye562@yahoo.com>
2024-06-08 13:02:04 -0700

    Update Security update button

--- frontend/src/pages/Settings/Security.tsx
+++ frontend/src/pages/Settings/Security.tsx
@@ -75,7 +75,7 @@ const Security = ({
 
           <button
             type="submit"
-            className="rounded-md border border-orange-500 px-3 py-2 text-sm text-orange-500 hover:text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+            className="rounded-md border border-orange-500 px-6 lg:px-10 py-2 lg:py-3 text-sm lg:text-base text-orange-500 hover:text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
           >
             Update password
           </button>
