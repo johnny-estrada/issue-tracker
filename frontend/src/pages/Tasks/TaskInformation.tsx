a0dd1db3191860804785db354476dbf67882869a
Johnny Estrada <johnnye562@yahoo.com>
2024-06-14 22:28:11 -0700

    Refactor TaskInformation

--- frontend/src/pages/Tasks/TaskInformation.tsx
+++ frontend/src/pages/Tasks/TaskInformation.tsx
@@ -3,12 +3,12 @@ import { useGetTaskQuery } from "../../state/redux/slices/tasksApiSlice";
 import { useGetProjectsQuery } from "../../state/redux/slices/projectsApiSlice";
 import { useGetUsersQuery } from "../../state/redux/slices/usersApiSlice";
 import HeaderTitle from "../../components/ui/header/HeaderTitle";
-import ButtonGroup from "../../components/ui/header/ButtonGroup";
+import ButtonGroup from "./components/ButtonGroup";
 import TwoColumns from "../../layout/TwoColumns";
-import SortBy from "../../components/ui/header/SortBy";
+import LabeledSelect from "../../components/form/LabeledSelect";
 import SearchBar from "../../components/ui/header/SearchBar";
 import Subtasks from "./components/Subtasks";
-import Comments from "./components/comments";
+// import Comments from "./components/comments";
 import Information from "./components/Information";
 import Details from "./components/Details";
 import Loader from "../../components/ui/Loader";
@@ -16,63 +16,72 @@ import { useAppSelector } from "../../hooks/hooks";
 
 interface Task {
   id: string;
+  priority: string;
 }
 
 const TaskInformation = () => {
   const { id } = useParams();
-
   const { data: projects, isLoading } = useGetProjectsQuery("");
   const { data: tasks, isLoading: isLoadingTasks } = useGetTaskQuery("");
   const { data: users, isLoading: isLoadingUsers } = useGetUsersQuery("");
   const { userInfo } = useAppSelector((state) => state.auth);
 
-  const b = tasks?.findIndex((task: Task) => task.id == id);
+  if (isLoading || isLoadingTasks || isLoadingUsers) {
+    return <Loader />;
+  }
 
-  if (!users) {
+  const taskIndex = tasks?.findIndex((task: Task) => task.id == id);
+  const task = tasks[taskIndex];
+
+  if (!task || !users) {
     return null;
   }
 
-  if (!tasks) {
-    return null;
-  }
+  const handlePriorityChange = (priority: string) => {
+    // Update task priority logic here
+    // For example, you can make an API call to update the task priority in your backend
+    console.log("Priority changed to:", priority);
+  };
 
   return (
     <>
-      {isLoading && <Loader />}
-      {isLoadingTasks && <Loader />}
-      {isLoadingUsers && <Loader />}
-
       <div>
         <TwoColumns>
-          <HeaderTitle title={tasks[b].name} active={true} />
+          <HeaderTitle title={task.name} active={true} />
           <SearchBar />
-          <div className="flex justify-between">
-            <div className="flex gap-10">
-              <SortBy />
-              <div className="text-gray-600 text-sm">
-                Priority:{" "}
-                <ButtonGroup
-                  titles={["high", "medium", "low"]}
-                  onFilter={() => {}}
-                />
-              </div>
+
+          <div className="flex gap-4 lg:gap-10">
+            <LabeledSelect
+              label="Status:"
+              options={["backlog", "to do", "in progress", "done"]}
+              active={task.status}
+            />
+            <div>
+              <span className="text-gray-600 text-xs lg:text-sm lg:mr-2">
+                Priority:
+              </span>
+              <ButtonGroup
+                titles={["high", "medium", "low"]}
+                initialActive={task.priority}
+                onClick={handlePriorityChange}
+              />
             </div>
           </div>
 
-          <div className="flex-1 p-4 lg:p-0">
-            <Information tasks={tasks} taskIndex={b} />
+          <div className="flex-1 py-4 lg:py-0">
+            <Information tasks={tasks} taskIndex={taskIndex} />
             <Subtasks />
-            {/* <Comments b={b} tasks={tasks} /> */}
+            {/* <Comments b={taskIndex} tasks={tasks} /> */}
           </div>
 
           <div className="flex-1 mb-20 p-4 lg:p-0">
-            <section aria-labelledby="tasks">
+            <section aria-labelledby="tasks" className="hidden lg:block">
               <Details
                 userId={userInfo.id}
                 taskId={id || ""}
                 users={users || undefined}
                 tasks={tasks}
-                taskIndex={b}
+                taskIndex={taskIndex}
                 projects={projects}
               />
             </section>
