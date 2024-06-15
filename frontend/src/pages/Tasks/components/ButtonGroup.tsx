9f779cd8cc2ca87438c995faacdfb66786783e4b
Johnny Estrada <johnnye562@yahoo.com>
2024-06-14 22:29:10 -0700

    Create ButtonGroup component

---
+++ frontend/src/pages/Tasks/components/ButtonGroup.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+interface Props {
+  titles: string[];
+  initialActive?: string;
+  onClick: (title: string) => void;
+}
+
+export default function ButtonGroup({ titles, initialActive, onClick }: Props) {
+  const [active, setActive] = useState(initialActive || titles[0]);
+
+  return (
+    <div className="isolate inline-flex rounded-md">
+      {titles.map((title) => (
+        <button
+          key={title}
+          type="button"
+          onClick={() => {
+            setActive(title); // Set the active button
+            onClick(title); // Trigger the onClick function
+          }}
+          className={`relative inline-flex items-center first:rounded-l-md last:rounded-r-md px-2 py-0 lg:px-3 lg:py-2 text-xs lg:text-sm w-fit h-8 ${
+            active === title
+              ? "bg-stone-800 text-white ring-1 ring-inset ring-stone-800 shadow-sm pointer-events-none"
+              : "bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          } focus:z-10`}
+        >
+          {title}
+        </button>
+      ))}
+    </div>
+  );
+}
