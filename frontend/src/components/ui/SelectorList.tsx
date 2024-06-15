0753a78ed635f1e6924a7e01ba5d96e53873e877
Johnny Estrada <johnnye562@yahoo.com>
2024-06-14 22:19:30 -0700

    Minor color update to SelectorList

--- frontend/src/components/ui/SelectorList.tsx
+++ frontend/src/components/ui/SelectorList.tsx
@@ -18,7 +18,7 @@ export default function SelectorList({ children, id, active, onClick }: Props) {
             className={
               active
                 ? "pointer-events-none flex flex-wrap  justify-between items-center gap-x-6 px-2 lg:px-2 py-2 lg:py-3 sm:px-6 bg-neutral-800  rounded-lg shadow ring-1 ring-gray-900/5 text-white w-full"
-                : " flex-wrap w-full flex cursor-pointer items-center justify-between gap-x-6 px-2 lg:px-2 py-2 lg:py-3 hover:bg-gray-200 sm:px-6 bg-gray-100 rounded-lg shadow ring-1 ring-gray-200 text-neutral-500"
+                : " flex-wrap w-full flex cursor-pointer items-center justify-between gap-x-6 px-2 lg:px-2 py-2 lg:py-3 hover:bg-gray-200 sm:px-6 bg-gray-100 rounded-lg shadow ring-1 ring-gray-200 text-gray-800"
             }
           >
             {children}
