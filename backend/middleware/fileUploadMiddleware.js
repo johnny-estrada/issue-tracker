162953929af1c4c12cb49e68208eb2d48c60478f
Johnny Estrada <johnnye562@yahoo.com>
2024-07-16 21:10:25 -0700

    Update destination in fil

--- backend/middleware/fileUploadMiddleware.js
+++ backend/middleware/fileUploadMiddleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public/Uploads/");
+    cb(null, "./backend/Uploads");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "_" + file.originalname);
