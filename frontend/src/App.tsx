e831d1b268de956bd9dc7f8b735f96926543f489
Johnny Estrada <johnnye562@yahoo.com>
2024-11-05 18:24:26 -0800

    Add overlay and new pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,53 +2,51 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import store from "./state/redux/store";
+import store from "./redux/store";
 import PrivateRoute from "./components/PrivateRoute";
-import Dashboard from "./pages/Dashboard";
+import Dashboard1 from "./pages/Dashboard/Dashboard1";
+import Clients from "./pages/Clients";
 import Projects from "./pages/Projects";
-import EditProject from "./pages/Projects/EditProject";
-import CreateProject from "./pages/Projects/CreateProject";
 import Tasks from "./pages/Tasks";
-import CreateTask from "./pages/Tasks/CreateTask";
-import EditTask from "./pages/Tasks/EditTask";
-import TaskInformation from "./pages/Tasks/TaskInformation";
+import TaskInformation from "./pages/Tasks/MainTaskDetails";
+import TimeTracker from "./pages/TimeTracker";
 import Notifications from "./pages/Notifications";
 import Settings from "./pages/Settings";
 import Login from "./pages/authentication/Login";
 import SignUp from "./pages/authentication/SignUp";
-import TestForm from "./pages/TestForm";
+import { OverlayProvider } from "./services/providers/OverlayContext";
+import Support from "./pages/Support";
 
 function App() {
   return (
     <BrowserRouter>
-      <Provider store={store}>
-        <ToastContainer />
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
+      <OverlayProvider>
+        <Provider store={store}>
+          <ToastContainer />
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
 
-          {/* Private Routes */}
-          <Route path="" element={<PrivateRoute />}>
-            <Route index={true} path="/" element={<Dashboard />} />
+            {/* Private Routes */}
+            <Route path="" element={<PrivateRoute />}>
+              <Route index={true} path="/" element={<Dashboard1 />} />
 
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/projects/create" element={<CreateProject />} />
-            <Route path="/projects/edit/:id" element={<EditProject />} />
+              <Route path="/clients" element={<Clients />} />
+              <Route path="/projects" element={<Projects />} />
 
-            <Route path="/tasks" element={<Tasks />} />
-            <Route path="/tasks/:id" element={<TaskInformation />} />
-            <Route path="/tasks/create" element={<CreateTask />} />
-            <Route path="/tasks/edit/:id" element={<EditTask />} />
+              <Route path="/tasks" element={<Tasks />} />
+              <Route path="/tasks/:id" element={<TaskInformation />} />
+              <Route path="/time-tracker" element={<TimeTracker />} />
 
-            <Route path="/notifications" element={<Notifications />} />
+              <Route path="/notifications" element={<Notifications />} />
+              <Route path="/support" element={<Support />} />
 
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/test" element={<TestForm />} />
-            <Route path="/upload" element={<TestForm />} />
-          </Route>
-        </Routes>
-      </Provider>
+              <Route path="/settings" element={<Settings />} />
+            </Route>
+          </Routes>
+        </Provider>
+      </OverlayProvider>
     </BrowserRouter>
   );
 }
