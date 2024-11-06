9ea7a883c2900ac9034b86d7ace25521299e3710
Johnny Estrada <johnnye562@yahoo.com>
2024-11-05 18:32:10 -0800

    Update redux imports

--- frontend/src/pages/authentication/Login.tsx
+++ frontend/src/pages/authentication/Login.tsx
@@ -1,8 +1,8 @@
 import { useState, useEffect, SyntheticEvent } from "react";
 import { Link, useNavigate, NavLink } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { useLoginMutation } from "../../state/redux/slices/usersApiSlice";
-import { setCredentials } from "../../state/redux/slices/authSlice";
+import { useLoginMutation } from "../../redux/slices/usersApiSlice";
+import { setCredentials } from "../../redux/slices/authSlice";
 import { toast } from "react-toastify";
 import Loader from "../../components/ui/Loader";
 import { useAppSelector } from "../../hooks/hooks";
@@ -224,7 +224,7 @@ export default function Login() {
         <div className="relative hidden w-0 flex-1 lg:block">
           <img
             className="absolute inset-0 h-full w-full object-cover"
-            src="./brooke-cagle-tLG2hcpITZE-unsplash.jpg"
+            src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
             alt=""
           />
         </div>
